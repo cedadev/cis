6d4d115b736be2c2e749b71acf8a8962b1daba00
Daniel Wallis <daniel.wallis@tessella.com>
2013-01-14 14:01:14 +0000

    Create InvalidDimensionError and InvalidVariableError

---
+++ Exceptions/InvalidDimensionError.py
@@ -0,0 +1,7 @@
+# InvalidDimensionError.py
+# Created by WALDM on 14th Jan 2013
+# Copyright TODO
+#
+# Error raised when an invalid dimension is entered
+class InvalidDimensionError(Exception):
+    pass
---
+++ Exceptions/InvalidVariableError.py
@@ -0,0 +1,7 @@
+# InvalidVariableError.py
+# Created by WALDM on 14th Jan 2013
+# Copyright TODO
+#
+# Error raised when an invalid variable is entered
+class InvalidVariableError(Exception):
+    pass
