9a68f1ef449c781e946d2328e315ffe646ad15b6
Daniel Wallis <daniel.wallis@tessella.com>
2013-01-15 15:46:15 +0000

    Remove unnecessary checking from Plotter and add in Iris plot functionality

--- Plotting/Plotter.py
+++ Plotting/Plotter.py
@@ -3,18 +3,24 @@
 # Copyright TODO
 #
 # Class for plotting graphs
-from Exceptions.InvalidDimensionError import InvalidDimensionError
-from Exceptions.InvalidVariableError import InvalidVariableError
-import Controller
 
-class Plotter():   
-    def plot1D(self, filename, variable):
-        if (variable not in Controller.variables[0][:]):
-            raise InvalidVariableError("'" + variable + "' cannot be found in '" + filename + "'")
-        else:
-            indexOfVariable = Controller.variables[0][:].index(variable)
-            dimensionsOfVariable = Controller.variables[1][indexOfVariable]
-            
-            if (dimensionsOfVariable != 1):
-                raise InvalidDimensionError("'" + variable + "' does not have one dimension")
+'''from Exceptions.InvalidDimensionError import InvalidDimensionError
+from Exceptions.InvalidVariableError import InvalidVariableError
+import Controller'''
+import iris.plot as iplt
+import matplotlib.pyplot as plt
+
+#class Plotter():   
+    #def plot1D(self, data, kwargs**):
+def plot1D(data):
+    iplt.plot(data)    
+    plt.show()
+    '''if (variable not in Controller.variables[0][:]):
+        raise InvalidVariableError("'" + variable + "' cannot be found in '" + filename + "'")
+    else:
+        indexOfVariable = Controller.variables[0][:].index(variable)
+        dimensionsOfVariable = Controller.variables[1][indexOfVariable]
+        
+        if (dimensionsOfVariable != 1):
+            raise InvalidDimensionError("'" + variable + "' does not have one dimension")'''
         
