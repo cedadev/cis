45738d82dc775704a3e032b240a160647a6b469f
Daniel Wallis <daniel.wallis@tessella.com>
2013-01-15 15:47:35 +0000

    Test the parser

--- UnitTests/TestParser.py
+++ UnitTests/TestParser.py
@@ -7,6 +7,9 @@ from nose.tools import istest, raises
 import Parser
 from UnitTests.StringsUsedInTests import valid_filename, valid_variable, invalid_filename, netcdf_file_with_incorrect_file_extension
 from Exceptions.InvalidFilenameError import InvalidFilenameError
+from Exceptions.InvalidChartTypeError import InvalidChartTypeError
+from Exceptions.NoVariablesSpecifiedError import NoVariablesSpecifiedError
+import Controller
 
 @istest
 def can_specify_one_valid_filename():
@@ -15,7 +18,7 @@ def can_specify_one_valid_filename():
     
 @istest
 @raises(InvalidFilenameError)
-def should_raise_error_with_one_invalid_filename():
+def should_raise_invalidfilenameerror_with_one_invalid_filename():
     args = [invalid_filename, "-v", valid_variable]
     Parser.parse_args(args)   
     
@@ -23,3 +26,43 @@ def should_raise_error_with_one_invalid_filename():
 def can_specify_more_than_one_valid_filename():
     args = [valid_filename, netcdf_file_with_incorrect_file_extension, "-v", valid_variable]
     Parser.parse_args(args)
+    
+@istest
+@raises(InvalidFilenameError)
+def should_raise_invalidfilenameerror_with_a_mixture_of_valid_and_invalid_filenames():
+    args = [valid_filename, invalid_filename, "-v", valid_variable]
+    Parser.parse_args(args)
+    
+@istest
+def can_specify_valid_chart_type():
+    args = [valid_filename, "-v", valid_variable, "--type", Controller.chart_types[0]]
+    Parser.parse_args(args)
+
+@istest
+@raises(InvalidChartTypeError)
+def should_raise_invalidcharttypeerror_with_an_invalid_chart_type():
+    args = [valid_filename, "-v", valid_variable, "--type", "dfgdfgdfgdfgdfgdf"]
+    Parser.parse_args(args)
+    
+@istest
+def should_raise_error_with_more_than_one_chart_type():
+    try:
+        args = [valid_filename, "-v", valid_variable, "--type", Controller.chart_types[0], Controller.chart_types[1]]
+        Parser.parse_args(args)
+        assert False
+    except SystemExit as e:
+        if e.code != 2:
+            raise e
+
+@istest
+def can_specify_more_than_one_variable():
+    args = [valid_filename, "-v", valid_variable, valid_variable]
+    Parser.parse_args(args)
+    
+@istest
+@raises(NoVariablesSpecifiedError)
+def should_raise_novariablesspecifiederror_when_no_variable_is_specified():
+    args = [valid_filename]
+    Parser.parse_args(args)
+
+#how to specify more than one variable
