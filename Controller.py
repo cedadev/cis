f6e2ed7f695db7ee7ee622fb1c2805a02b7a2d82
Daniel Wallis <daniel.wallis@tessella.com>
2013-01-15 12:17:49 +0000

    Remove unnecessary imports from Controller

--- Controller.py
+++ Controller.py
@@ -3,12 +3,9 @@
 # Copyright TODO
 #
 # Controller module
-import argparse
-import sys
-import os.path
 
 variables = [["nameofvariable", "not1Dvariable"], [1, 2]]
 chart_types = ["heatmap", "contour", "filled_contour", "line", "scatter"]
 
-def get_number_of_variables(file):
-    return len(file)
+def get_number_of_variables(netcdf_file):
+    return len(netcdf_file)
