7ea1f90a04c214ff00e612b3e7996c85600f73e8
Daniel Wallis <daniel.wallis@tessella.com>
2013-01-16 11:22:49 +0000

    Test line graph plot

--- Plotting/Plotter.py
+++ Plotting/Plotter.py
@@ -10,19 +10,24 @@ import matplotlib.pyplot as plt
 from UnitTests.StringsUsedInTests import valid_variable_in_valid_filename, valid_filename
 import Parser
    
-def plot1D(data, out_filename = None, *args, **kwargs):    
-    iplt.plot(data, *args, **kwargs)    
+def plotLineGraph(data, out_filename = None, *args, **kwargs):    
+    iplt.plot(data, *args, **kwargs)   
+     
     if out_filename == None:
         plt.show()
     else:
+        # Will overwrite if file already exists
         plt.savefig(out_filename)
 
-args = Parser.parse_args([valid_filename, "-v", valid_variable_in_valid_filename])
+def main():
+    args = Parser.parse_args([valid_filename, "-v", valid_variable_in_valid_filename])
 
-variable = iris.AttributeConstraint(name = args.variables[0]) 
-cube = iris.load_cube(args.filenames, variable)
-cube = list(cube.slices([coord for coord in cube.coords() if coord.points.size > 1]))[0] 
+    variable = iris.AttributeConstraint(name = args.variables[0]) 
+    cube = iris.load_cube(args.filenames, variable)
+    cube = list(cube.slices([coord for coord in cube.coords() if coord.points.size > 1]))[0] 
 
-kwargs = {"title" : "My title"}
-plot1D(cube, **kwargs)
+    kwargs = {"title" : "My title"}
+    plot1D(cube, **kwargs)
+    
+#main()
         
--- UnitTests/Plot1DNetCDFFile.py
+++ UnitTests/Plot1DNetCDFFile.py
@@ -4,16 +4,29 @@
 #
 # Module to test the one-dimensional plotting of NetCDF files
 from Plotting import Plotter
-from nose.tools import istest
-from UnitTests.StringsUsedInTests import valid_filename, valid_variable_in_valid_filename
+from nose.tools import istest, raises
+from UnitTests.StringsUsedInTests import valid_filename, valid_variable_in_valid_filename, out_filename
 import iris
+import os.path
 
 @istest
-def can_plot_cube():    
+def can_plot_cube_to_file():    
+    if os.path.isfile(out_filename):
+        os.remove(out_filename)
+    
     variable = iris.AttributeConstraint(name = valid_variable_in_valid_filename)
     cube = iris.load_cube(valid_filename, variable) 
     cube = list(cube.slices([ coord for coord in cube.coords() if coord.points.size > 1]))[0]   
-    Plotter.plot1D(cube)
-
+    Plotter.plotLineGraph(cube, out_filename)
+    
+    assert(os.path.isfile(out_filename))
+    
+@istest
+@raises(IOError)
+def should_raise_io_error_with_invalid_filename():    
+    variable = iris.AttributeConstraint(name = valid_variable_in_valid_filename)
+    cube = iris.load_cube(valid_filename, variable) 
+    cube = list(cube.slices([ coord for coord in cube.coords() if coord.points.size > 1]))[0]   
+    Plotter.plotLineGraph(cube, "/")
 
     
