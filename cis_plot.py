519cfd42ef4cdd59f9c36636e8062b6883e2738b
Duncan Watson-Parris <duncan.watson-parris@tessella.com>
2013-01-16 12:11:19 +0000

    Updated parser to give seperate namespaces for the main arguments and the plot arguments.

--- cis_plot.py
+++ cis_plot.py
@@ -2,27 +2,25 @@
 '''
 Main driver script for the plotting tool
 '''
-from Plotting.Plotter import plot1D, heatmap
+from Plotting.Plotter import plot_line_graph, plot_heatmap
 from Parser import parse_args
 from Reading.DataReader import read_variable
 from Exceptions.InvalidDimensionError import InvalidDimensionError
 
-plot_types = {'line_plot' : plot1D, 
-                'heatmap' : heatmap }
+plot_types = {'line_plot' : plot_line_graph, 
+                'heatmap' : plot_heatmap }
 
 if __name__ ==  '__main__':
-    arguments = parse_args()
+    main_arguments, plot_args = parse_args()
     
     try:
-        data = read_variable(arguments.filenames, arguments.variable)
+        data = read_variable(main_arguments.filenames, main_arguments.variable)
     except:
         pass
         # Think about what to catch here
     
-    arguments.
-    
     try:
-        plot_types[arguments.type](data, *arguments.formating)
+        plot_types[main_arguments.type](data, main_arguments.output, *plot_args)
     except KeyError:
         print "Invalid plot type"
         exit(1)
