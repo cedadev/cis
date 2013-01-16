013fb8ca9a86144ae3bf986e4a0475c9e8956b26
Daniel Wallis <daniel.wallis@tessella.com>
2013-01-16 10:22:48 +0000

    Move string references to shared folder

--- UnitTests/StringsUsedInTests.py
+++ UnitTests/StringsUsedInTests.py
@@ -3,12 +3,12 @@
 # Copyright TODO
 #
 # Module that contains various strings that are used in tests
-valid_filename = "/home/daniel/NetCDF Files/xglnwa.pm.k8dec-k9nov.vprof.tm.nc"
+valid_filename = "/home/shared/NetCDF Files/xglnwa.pm.k8dec-k9nov.vprof.tm.nc"
 invalid_filename = "invalidfilename"
-non_netcdf_file = "/home/daniel/Desktop/Connect to Jasmin"
-file_without_read_permissions = "/home/daniel/NetCDF Files/Unreadable Folder/xglnwa.pm.k8dec-k9nov.vprof.tm.nc"
-netcdf_file_with_incorrect_file_extension = "/home/daniel/NetCDF Files/xglnwa.pm.k8dec-k9nov.vprof.tm.waldm"
-non_netcdf_file_with_netcdf_file_extension = "/home/daniel/NetCDF Files/notarealnetcdffile.nc"
+non_netcdf_file = "/home/shared/Connect to Jasmin"
+file_without_read_permissions = "/home/shared/NetCDF Files/Unreadable Folder/xglnwa.pm.k8dec-k9nov.vprof.tm.nc"
+netcdf_file_with_incorrect_file_extension = "/home/shared/NetCDF Files/xglnwa.pm.k8dec-k9nov.vprof.tm.waldm"
+non_netcdf_file_with_netcdf_file_extension = "/home/shared/NetCDF Files/notarealnetcdffile.nc"
 valid_variable = "nameofvariable"
 invalid_variable = "invalidvariable"
 not1Dvariable = "not1Dvariable" 
