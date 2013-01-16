c420e5c924384ba02cc04510d6931403ec2d3b61
Daniel Wallis <daniel.wallis@tessella.com>
2013-01-16 10:28:41 +0000

    Rename non_netcdf_file

--- UnitTests/StringsUsedInTests.py
+++ UnitTests/StringsUsedInTests.py
@@ -5,7 +5,7 @@
 # Module that contains various strings that are used in tests
 valid_filename = "/home/shared/NetCDF Files/xglnwa.pm.k8dec-k9nov.vprof.tm.nc"
 invalid_filename = "invalidfilename"
-non_netcdf_file = "/home/shared/Connect to Jasmin"
+non_netcdf_file = "/home/shared/NetCDF Files/notanetcdffile"
 file_without_read_permissions = "/home/shared/NetCDF Files/Unreadable Folder/xglnwa.pm.k8dec-k9nov.vprof.tm.nc"
 netcdf_file_with_incorrect_file_extension = "/home/shared/NetCDF Files/xglnwa.pm.k8dec-k9nov.vprof.tm.waldm"
 non_netcdf_file_with_netcdf_file_extension = "/home/shared/NetCDF Files/notarealnetcdffile.nc"
