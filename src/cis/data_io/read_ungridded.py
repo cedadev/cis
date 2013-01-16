a45294a93298efb558cd5ac63861281c4281c6cd
Duncan Watson-Parris <duncan.watson-parris@tessella.com>
2013-01-16 17:12:40 +0000

    Adding renamed data io package

---
+++ src/cis/data_io/read_ungridded.py
@@ -0,0 +1,15 @@
+# UngriddedDataReader.py
+# Created by PARD on 14th Jan 2013
+# Copyright TODO
+#
+# Class for ungridded data and utilities for handling it
+
+def read_ungridded_data(filenames, variables):
+    pass
+
+class UngriddedData():
+    def __init__(self, data=None):
+        self._data = data
+        # numpy array for the actual data? Maybe see sat.hdf.py
+        
+        
