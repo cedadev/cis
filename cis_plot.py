a843275052926338794cfecfa285f779117a8220
Duncan Watson-Parris <duncan.watson-parris@tessella.com>
2013-01-16 12:14:29 +0000

    Updated with new plot types

--- cis_plot.py
+++ cis_plot.py
@@ -2,12 +2,13 @@
 '''
 Main driver script for the plotting tool
 '''
-from Plotting.Plotter import plot_line_graph, plot_heatmap
+from Plotting.Plotter import plot_line_graph, plot_heatmap, plot_scatter_graph
 from Parser import parse_args
 from Reading.DataReader import read_variable
 from Exceptions.InvalidDimensionError import InvalidDimensionError
 
-plot_types = {'line_plot' : plot_line_graph, 
+plot_types = {'line_plot' : plot_line_graph,
+                'scatter' : plot_scatter_graph, 
                 'heatmap' : plot_heatmap }
 
 if __name__ ==  '__main__':
