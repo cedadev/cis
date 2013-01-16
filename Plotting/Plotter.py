3465e8584d4a93254db02b7f0f67dba956a9a2fd
Daniel Wallis <daniel.wallis@tessella.com>
2013-01-16 10:05:56 +0000

    Remove old commented out code

--- Plotting/Plotter.py
+++ Plotting/Plotter.py
@@ -4,23 +4,25 @@
 #
 # Class for plotting graphs
 
-'''from Exceptions.InvalidDimensionError import InvalidDimensionError
-from Exceptions.InvalidVariableError import InvalidVariableError
-import Controller'''
+import iris
 import iris.plot as iplt
 import matplotlib.pyplot as plt
-
-#class Plotter():   
-    #def plot1D(self, data, kwargs**):
-def plot1D(data):
-    iplt.plot(data)    
-    plt.show()
-    '''if (variable not in Controller.variables[0][:]):
-        raise InvalidVariableError("'" + variable + "' cannot be found in '" + filename + "'")
+from UnitTests.StringsUsedInTests import valid_variable_in_valid_filename, valid_filename
+import Parser
+   
+def plot1D(data, out_filename = None, *args, **kwargs):    
+    iplt.plot(data, *args, **kwargs)    
+    if out_filename == None:
+        plt.show()
     else:
-        indexOfVariable = Controller.variables[0][:].index(variable)
-        dimensionsOfVariable = Controller.variables[1][indexOfVariable]
-        
-        if (dimensionsOfVariable != 1):
-            raise InvalidDimensionError("'" + variable + "' does not have one dimension")'''
+        plt.savefig(out_filename)
+
+args = Parser.parse_args([valid_filename, "-v", valid_variable_in_valid_filename])
+
+variable = iris.AttributeConstraint(name = args.variables[0]) 
+cube = iris.load_cube(args.filenames, variable)
+cube = list(cube.slices([coord for coord in cube.coords() if coord.points.size > 1]))[0] 
+
+kwargs = {"title" : "My title"}
+plot1D(cube, **kwargs)
         
