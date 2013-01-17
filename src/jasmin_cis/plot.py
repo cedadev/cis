907df6968478c5b64afa1ce3b613a35e07406d67
Daniel Wallis <daniel.wallis@tessella.com>
2013-01-17 14:23:36 +0000

    Only plot coast lines if type supports it

--- src/jasmin_cis/plot.py
+++ src/jasmin_cis/plot.py
@@ -53,6 +53,9 @@ def plot(data, plot_type = None, out_filename = None, options = None, *args, **k
     elif plot_types[plot_type].variable_dimensions != variable_dim:
         raise ex.InvalidPlotTypeError("The plot type is not valid for this variable, the dimensions do not match")
     
+    if plot_types[plot_type].expected_no_of_variables != len(data):
+        raise ex.InvalidPlotTypeError("The plot type is not valid for these variables")   
+    
     try:
         plot_types[plot_type].plot_method(data, *args, **kwargs)
     except KeyError:
@@ -68,7 +71,11 @@ def plot(data, plot_type = None, out_filename = None, options = None, *args, **k
                 # should include all of the valid formatting options
     
     if plot_types[plot_type].is_map:
-        plt.gca().coastlines()    
+        # Try and add the coast lines if the map supports it
+        try:
+            plt.gca().coastlines()
+        except AttributeError:
+            pass
          
     if out_filename == None:
         plt.show()  
