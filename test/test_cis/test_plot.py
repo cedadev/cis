2aa3c0b1fd288c1e3fc5b21fd2dd52fde88b97cf
Daniel Wallis <daniel.wallis@tessella.com>
2013-01-18 14:00:31 +0000

    Test the plot module

--- test/test_cis/test_plot.py
+++ test/test_cis/test_plot.py
@@ -6,6 +6,7 @@ from nose.tools import istest, raises
 from test_cis.data import *
 import iris
 import os.path
+from jasmin_cis.exceptions import *
 
 def delete_file_if_exists():
     '''
@@ -15,8 +16,9 @@ def delete_file_if_exists():
     if os.path.isfile(out_filename):
         os.remove(out_filename)
 
-def make_cube(filename):
-    variable = iris.AttributeConstraint(name = valid_variable_in_valid_filename)
+def make_cube(filename, variable = None):
+    if variable == None:
+        variable = iris.AttributeConstraint(name = valid_variable_in_valid_filename)
     cube = iris.load_cube(filename, variable) 
     cube = list(cube.slices([ coord for coord in cube.coords() if coord.points.size > 1]))[0]
     return cube       
@@ -25,70 +27,80 @@ def make_cube(filename):
 def can_plot_line_graph_to_file():    
     delete_file_if_exists()
     cube = make_cube(valid_1d_filename)
-    plot(cube, "line", out_filename)   
+    plot([cube], "line", out_filename)   
     assert(os.path.isfile(out_filename))
 
 # Not an automated test   
 def can_plot_line_graph_to_screen():    
     cube = make_cube(valid_1d_filename)       
-    plot(cube, "line")
+    plot([cube], "line")
     
 @istest
 def can_plot_scatter_graph_to_file():    
     delete_file_if_exists()
     cube = make_cube(valid_2d_filename)
-    plot(cube, "scatter", out_filename)     
+    plot([cube], "scatter", out_filename)     
     assert(os.path.isfile(out_filename))
     
 # Not an automated test    
 def can_plot_scatter_graph_to_screen():    
     cube = make_cube(valid_2d_filename)       
-    plot(cube, "scatter")
+    plot([cube], "scatter")
 
 @istest
 def can_plot_heatmap_to_file():    
     delete_file_if_exists()
     cube = make_cube(valid_2d_filename)
-    plot(cube, "heatmap", out_filename)   
+    plot([cube], "heatmap", out_filename)   
     assert(os.path.isfile(out_filename))
    
 # Not an automated test   
 def can_plot_heatmap_to_screen():    
     cube = make_cube(valid_2d_filename)       
-    plot(cube, "heatmap")
+    plot([cube], "heatmap")
     
 @istest
 def can_plot_contour_to_file():    
     delete_file_if_exists()
     cube = make_cube(valid_2d_filename)
-    plot(cube, "contour", out_filename)   
+    plot([cube], "contour", out_filename)   
     assert(os.path.isfile(out_filename))
    
 # Not an automated test   
 def can_plot_contour_to_screen():    
     cube = make_cube(valid_2d_filename)       
-    plot(cube, "contour")
+    plot([cube], "contour")
 
 @istest
 def can_plot_contourf_to_file():    
     delete_file_if_exists()
     cube = make_cube(valid_2d_filename)
-    plot(cube, "contourf", out_filename)   
+    plot([cube], "contourf", out_filename)   
     assert(os.path.isfile(out_filename))
    
 # Not an automated test   
 def can_plot_contourf_to_screen():    
     cube = make_cube(valid_2d_filename)       
-    plot(cube, "contourf")
+    plot([cube], "contourf")
 
     
 @istest
 @raises(IOError)
 def should_raise_io_error_with_invalid_filename():     
     cube = make_cube(valid_1d_filename)
-    plot(cube, "line", "/")
+    plot([cube], "line", "/")
+   
+@istest
+@raises(InvalidDimensionError)
+def should_raise_error_when_variable_is_not_1D():
+    delete_file_if_exists()
+    cube = make_cube(valid_1d_filename, not1Dvariable)
+    plot([cube], "line", out_filename)   
+    assert(os.path.isfile(out_filename))
+    
     
 if __name__ == "__main__":
+    # Call the methods to plot to screen instead of the plot to file ones
     can_plot_line_graph_to_screen()
     can_plot_scatter_graph_to_screen()
     can_plot_heatmap_to_screen()
