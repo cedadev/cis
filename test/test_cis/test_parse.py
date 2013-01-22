2eebc2ce3da99a931250800681e5935ca21ab110
Daniel Wallis <daniel.wallis@tessella.com>
2013-01-22 16:55:07 +0000

    Add more tests

--- test/test_cis/test_parse.py
+++ test/test_cis/test_parse.py
@@ -14,7 +14,7 @@ def can_specify_one_valid_filename():
     parse_args(args)
     
 @istest
-def should_raise_invalidfilenameerror_with_one_invalid_filename():
+def should_raise_error_with_one_invalid_filename():
     try:
         args = ["plot", invalid_filename, "-v", valid_variable]
         parse_args(args)
@@ -29,7 +29,7 @@ def can_specify_more_than_one_valid_filename():
     parse_args(args)
     
 @istest
-def should_raise_invalidfilenameerror_with_a_mixture_of_valid_and_invalid_filenames():
+def should_raise_error_with_a_mixture_of_valid_and_invalid_filenames():
     try:
         args = ["plot", valid_1d_filename, invalid_filename, "-v", valid_variable]
         parse_args(args)
@@ -44,7 +44,7 @@ def can_specify_valid_chart_type():
     parse_args(args)
 
 @istest
-def should_raise_invalidcharttypeerror_with_an_invalid_chart_type():
+def should_raise_error_with_an_invalid_chart_type():
     try:
         args = ["plot", valid_1d_filename, "-v", valid_variable, "--type", "dfgdfgdfgdfgdfgdf"]
         parse_args(args)
@@ -69,7 +69,7 @@ def can_specify_more_than_one_variable():
     parse_args(args)
     
 @istest
-def should_raise_novariablesspecifiederror_when_no_variable_is_specified():
+def should_raise_error_when_no_variable_is_specified():
     try:
         args = ["plot", valid_1d_filename]
         parse_args(args)
@@ -77,5 +77,25 @@ def should_raise_novariablesspecifiederror_when_no_variable_is_specified():
     except SystemExit as e:
         if e.code != 2:
             raise e    
+        
+@istest
+def should_raise_error_with_invalid_line_width():
+    try:   
+        args = ["plot", valid_1d_filename, "-v", valid_variable, "--linewidth", "4a0"]
+        parse_args(args) 
+        assert False
+    except SystemExit as e:
+        if e.code != 2:
+            raise e  
+        
+@istest
+def should_raise_error_with_invalid_line_style():   
+    try:   
+        args = ["plot", valid_1d_filename, "-v", valid_variable, "--linestyle", "4a0"]
+        parse_args(args) 
+        assert False
+    except SystemExit as e:
+        if e.code != 2:
+            raise e 
 
 #how to specify more than one variable
--- test/test_cis/test_plot_format_options.py
+++ test/test_cis/test_plot_format_options.py
@@ -2,7 +2,8 @@
 Module to test the plot formatting options
 '''
 from jasmin_cis.plot import plot
-from nose.tools import istest
+from jasmin_cis.parse import parse_args
+from nose.tools import istest, raises
 import iris
 from data import *
 
@@ -16,5 +17,31 @@ def make_cube(filename, variable = None):
 @istest
 def can_set_line_colour_to_valid_colour():   
     cube = make_cube(valid_1d_filename)
-    plot([cube], "line", out_filename, **{"color" : "green"})  
+    plot([cube], out_filename = out_filename, **{"color" : "green"})
+    
+@istest
+@raises(ValueError)
+def should_raise_value_error_with_invalid_colour():   
+    cube = make_cube(valid_1d_filename)
+    plot([cube], out_filename = out_filename, **{"color" : "greenn"})
+    
+@istest
+def should_not_raise_error_if_colour_is_specified_for_invalid_chart_type(): # Simply discard the colour specified 
+    cube = make_cube(valid_2d_filename)
+    plot([cube], out_filename = out_filename, **{"color" : "greenn"})  
+    
+@istest
+def can_set_line_width_to_valid_width():   
+    cube = make_cube(valid_1d_filename)
+    plot([cube], out_filename = out_filename, **{"linewidth" : 40})   
+    
+@istest
+def can_set_line_style_to_valid_style():   
+    cube = make_cube(valid_1d_filename)
+    plot([cube], out_filename = out_filename, **{"linestyle" : "dashed"})   
+    
+@istest
+def can_set_colour_map_to_valid_colour():   
+    cube = make_cube(valid_2d_filename)
+    plot([cube], out_filename = out_filename, **{"cmap" : "RdBu"})
     
