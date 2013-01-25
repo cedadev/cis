0c54a30a6d0c3bfb77404394a62a1053c0a71ae9
David Michel <david.michel@tessella.com>
2013-01-25 12:38:43 +0000

    added new dependencies installation scripts for fedora and ubuntu + updated setup.py to reflect this + added bin with main cis executable

--- setup.py
+++ setup.py
@@ -1,29 +1,68 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+from pkg_resources import require, DistributionNotFound, VersionConflict
+import sys
+import os
+
+# Extension classes and functions to add custom command
+#======================================================
+
+
+class check_dep(Command):
+    """
+    Command to check that the required dependencies are installed on the system
+    """
+    description = "Checks that the required dependencies are installed on the system"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+
+        deps = ["matplotlib>=1.2.0","pyke","cartopy","Shapely","netcdf4",
+                "numpy","scipy","nose","iris","epydoc"]
+        for dep in deps:
+            try:
+                require(dep)
+                print dep + " ...[ok]"
+            except (DistributionNotFound, VersionConflict):
+                print dep + "... MISSING!" 
+
+class gen_doc(Command):
+    """
+    Command to generate the API reference with epydoc
+    """
+    description = "Generates the API reference with epydoc"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        raise NotImplementedError, "not implemented yet"       
+
+
+# Metadata description
+#=====================
 
 setup(
     name='cis',
     version='0.1',
     description='Climate Inter-comparison Suite',
-    author='David Michel',
-    author_email='david.michel@tessella.com',
-    url='www.tessella.com',
+    author=' ',
+    author_email=' ',
+    url='http://proj.badc.rl.ac.uk/cedaservices/wiki/JASMIN/CommunityIntercomparisonSuite',
 
     packages=find_packages(),
-
-    install_requires=[
-        "scipy",
-        "numpy",
-        "netCDF4",
-        "cartopy",
-        "Shapely",
-        "basemap",
-        "pyke",
-        "matplotlib==1.2.0",
-        "iris>=1.1.0"
-    ],
-
-    tests_require=["nose>=1.1.2"],
-
-    dependency_links=["https://github.com/SciTools/iris/tarball/master#egg=iris-1.1.0"]
+    scripts = ['bin/cis'],
+    
+    cmdclass={"gendoc": gen_doc, "checkdep":check_dep}
 )
+
 
