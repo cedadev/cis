9d3320355f4e2cd6d85186a6ce3481356d739d1b
Duncan Watson-Parris <duncan.watson-parris@tessella.com>
2013-01-28 10:25:09 +0000

    Refactored reading variables from a file when it's format isn't known.

--- src/jasmin_cis/data_io/read_gridded.py
+++ src/jasmin_cis/data_io/read_gridded.py
@@ -33,3 +33,18 @@ def read_gridded_data_file_variable(filenames, variable):
     #  shape (145, 165)
     
     return sub_cube
+
+
+def get_netcdf_file_variables(filename):
+    '''
+    Get all the variables from a NetCDF file
+    
+    args:
+        filename: The filename of the file to get the variables from
+    
+    returns:
+        An OrderedDict containing the variables from the file
+    '''
+    from netCDF4 import Dataset    
+    f = Dataset(filename)
+    return f.variables
