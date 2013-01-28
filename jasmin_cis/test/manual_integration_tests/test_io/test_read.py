409d884e1d6733a539dcec11b006a57ebc9b633e
Daniel Wallis <daniel.wallis@tessella.com>
2013-01-28 17:01:15 +0000

    Fix references to data strings file

--- jasmin_cis/test/manual_integration_tests/test_io/test_read.py
+++ jasmin_cis/test/manual_integration_tests/test_io/test_read.py
@@ -4,12 +4,12 @@ These tests have to be run manually
 '''
 from nose.tools import nottest
 import jasmin_cis.data_io.read as cis_read
-from test_files.data import *
+from jasmin_cis.test.test_files.data import *
 
 @nottest # Too big to store in Git repository, can still be run manually
 def can_read_15GB_file_when_reading_variables():
     filename = large_15GB_file_filename
-    cis_read.get_netcdf_file_variables(filename)
+    cis_read.read_file_variables(filename)
 
 @nottest # Too big to store in Git repository, can still be run manually
 def can_read_15GB_file_when_loading_a_cube():
@@ -19,3 +19,4 @@ def can_read_15GB_file_when_loading_a_cube():
 if __name__ == "__main__":
     can_read_15GB_file_when_reading_variables()
     can_read_15GB_file_when_loading_a_cube()
+    print("Finished")
