740c988f160191ee937f3f9fe8956f9150c47c73
Daniel Wallis <daniel.wallis@tessella.com>
2013-01-29 16:09:50 +0000

    Fix method name in test

--- jasmin_cis/test/manual_integration_tests/test_io/test_read.py
+++ jasmin_cis/test/manual_integration_tests/test_io/test_read.py
@@ -9,12 +9,12 @@ from jasmin_cis.test.test_files.data import *
 @nottest # Too big to store in Git repository, can still be run manually
 def can_read_15GB_file_when_reading_variables():
     filename = large_15GB_file_filename
-    cis_read.read_file_variables(filename)
+    cis_read.read_all_variables_from_file(filename)
 
 @nottest # Too big to store in Git repository, can still be run manually
 def can_read_15GB_file_when_loading_a_cube():
     filename = large_15GB_file_filename
-    cis_read.read_variable(filename, valid_variable_in_valid_filename)
+    cis_read.read_variable_from_files(filename, valid_variable_in_valid_filename)
 
 if __name__ == "__main__":
     can_read_15GB_file_when_reading_variables()
