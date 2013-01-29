d8738d9bc4c32c5d675a340c37f208bd7255edcd
Duncan Watson-Parris <duncan.watson-parris@tessella.com>
2013-01-29 15:29:00 +0000

    Rewrite of the colocation routines, col_cmd method and associated tests. There is now a colocation object called Colocator for performing the colocation, this gives us more flexibility in the arguments passed and the data returned.

--- jasmin_cis/col.py
+++ jasmin_cis/col.py
@@ -1,17 +1,26 @@
 '''
     Colocation routines - to be implemented
 '''
-import numpy as np
 from collections import namedtuple
 R_E = 6378
 
-HyperPointT = namedtuple('HyperPoint',['lat','lon','alt','t','val'])
+# Data type representing a point in space and time. It can contain multiple values which are stored in a list
+HyperPointT = namedtuple('HyperPoint',['latitude','longitude','altitude','time','val'])
 
 def HyperPoint(lat=None, lon=None, alt=None, t=None, val=None):
     '''
         Small constructor for the HyperPoint named tuple to allow optional arguments
+         and set-up value list.
     '''
+    # If no value was specified create an empty list, otherwise create a list with one entry   
+    if val is None:
+        val = []
+    else:
+        val = [ val ]
     return HyperPointT(lat,lon,alt,t,val)
+
+def get_coord_tuple(point):
+    return [ x for x, y in point._asdict().items() if y is not None and x != 'val' ]
 
 def compdist(ref_p,p1,p2):
     '''Compares the distance from reference point ref_p to p1 and p2. Returns True if p2 is closer to ref_p than p1'''
@@ -30,50 +39,80 @@ def haversine(lat1,lon1,lat2,lon2):
     arclen = 2*math.asin(math.sqrt((math.sin((lat2-lat1)/2))**2 + math.cos(lat1) * math.cos(lat2) * (math.sin((lon2-lon1)/2))**2))
     return arclen*R_E
 
-def col_nn(points, data):
+def furthest_point_from(lat,lon):
     '''
-        Colocation using nearest neighbours without any constraints.
-         This is just a skeleton at the moment - we need to define hoe
-         the data will come in
+        Return the furthest point along the surface of the globe from a given latitude and longitude
     '''
-    pass
-    # This doesn't quite work yet - I think we need to create a set of points with the same coordinates
-    #  as the sample points but without any values - these are what we're trying to fill in...
-#    new_data = np.array(points, np.zeros(points.shape,2))
-#    for point in new_data:
-#        nearest_point = LatLonPoint(0,0)
-#        for data_point in data:
-#            if compdist(data_point,nearest_point,point): nearest_point = point
-#        point.val = nearest_point.val
-    
+    if lat > 0:
+        furthest_lat = lat - 90.0
+    else:
+        furthest_lat = lat + 90.0
+    if lon > 180:
+        furthest_lon = lon - 180.0
+    else:
+        furthest_lon = lon + 180.0
+    return HyperPoint(furthest_lat, furthest_lon)
+
+class Colocator(object):
+
+    ColocationTechniques = namedtuple('Techniques',['nn', 'li'])
+    gridded_colocation_methods = ColocationTechniques(find_nn_value_gridded, find_value_by_li)
+    ungridded_colocation_methods = ColocationTechniques(find_nn_value_ungridded, None)
+
+    def __init__(self, points, data, col_method='nn', constraints=None):
+        from jasmin_cis.exceptions import InvalidColocationMethodError
+        from iris import cube
         
-def col_li(points, data):
-    pass
-
-def col_nn_wc(points, data):
-    pass
-
-colocation_methods = { 'nn' : col_nn, 
-                       'li' :col_li,
-                       'nn_wc' : col_nn_wc }
-
-def col(points, data, method='nn'):
-    from jasmin_cis.exceptions import InvalidColocationMethodError
-    try:
-        return colocation_methods[method](points,data)
-    except KeyError:
-        raise InvalidColocationMethodError
-
-def is_colocated(data1, data2):
-    '''
-        Checks wether two datasets share all of the same points, this might be useful
-        to determine if colocation is necesary or completed succesfully
-    '''
-    return np.array_equal(data1.points, data2.points)#
-    # Or manually?
-#    colocated = True
-#    for point1 in data1:
-#        colocated = all( point1 == point2 for point2 in data2 )
-#        if not colocated:
-#            return colocated
-#    return colocated
+        self.points = points
+        self.data = data
+        self.constraints = constraints        
+        
+        if isinstance(data, cube):  
+            methods = Colocator.gridded_colocation_methods
+        else:
+            methods = Colocator.ungridded_colocation_methods
+            
+        self.method = methods[col_method]
+               
+        if self.method is None:
+            raise InvalidColocationMethodError('This co-location method is invalid for this data type')
+                
+    def colocate(self):
+        for point in self.points:
+            point.val.append(self.method(point))
+        
+    def find_nn_value(self, point):
+        '''
+            Colocation using nearest neighbours without any constraints where both points and 
+              data are a list of HyperPoints
+        '''
+        nearest_point = furthest_point_from(point)
+        for data_point in self.data:
+            if compdist(data_point,nearest_point,point): nearest_point = data_point
+        return nearest_point.val
+    
+    def find_nn_value_ungridded(self, point, constraint_fn=None):
+        '''
+            Co-location routine using nearest neighbour algorithm optimized for ungridded data
+        '''
+        import numpy as np
+        nearest_point = furthest_point_from(point)
+        for (x,y), value in np.ndenumerate(self.data.vals):
+            ug_point = HyperPoint(self.data.lat[x,y],self.data.lon[x,y],val=value)
+            if compdist(ug_point,nearest_point,point): nearest_point = ug_point
+            
+        if constraint_fn is not None:
+            return constraint_fn(point, nearest_point)
+        else:
+            return nearest_point.val
+    
+    def find_nn_value_gridded(self, point):
+        '''
+            Co-location routine using nearest neighbour algorithm optimized for gridded data.
+             This calls out to iris to do the work.
+        '''
+        from iris.analysis.interpolate import nearest_neighbour_data_value
+        return nearest_neighbour_data_value(self.data, get_coord_tuple(point))
+    
+    def find_value_by_li(self, point):
+        pass
