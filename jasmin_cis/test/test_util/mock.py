e53696b32775a6384ad322b46daaf7018418d905
Duncan Watson-Parris <duncan.watson-parris@tessella.com>
2013-01-30 12:14:27 +0000

    Added new colocation tests

--- jasmin_cis/test/test_util/mock.py
+++ jasmin_cis/test/test_util/mock.py
@@ -20,16 +20,24 @@ def make_square_3x3_2d_cube():
         Makes a well defined cube of shape 3x3 with data as follows
         array([[1,2,3],
                [4,5,6],
-               [7,8,9]])
+               [7,8,9],
+               [10,11,12],
+               [13,14,15]])
+        and coordinates in latitude:
+            array([ -10, -5, 0, 5, 10 ])
+        longitude:
+            array([ -5, 0, 5 ])
+            
+        They are different lengths to make it easier to distinguish. Note the latitude increases
+        as you step through the array in order - so downwards as it's written above
     '''
     import numpy as np
     from iris.cube import Cube
     from iris.coords import DimCoord
     
-    latitude = DimCoord(range(-5, 5, 5), standard_name='latitude', units='degrees')
-    longitude = DimCoord(range(-5, 5, 5), standard_name='longitude', units='degrees')
-    data = np.arange(9)
-    np.reshape(data,3,3)
+    latitude = DimCoord(np.arange(-10, 11, 5), standard_name='latitude', units='degrees')
+    longitude = DimCoord(np.arange(-5, 6, 5), standard_name='longitude', units='degrees')
+    data = np.reshape(np.arange(15)+1,(5,3))
     cube = Cube(data, dim_coords_and_dims=[(latitude, 0), (longitude, 1)])
     
     return cube
