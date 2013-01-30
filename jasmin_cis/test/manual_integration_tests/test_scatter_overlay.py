6cbe69fac67260586a8be8b665db878ea69d3c94
Daniel Wallis <daniel.wallis@tessella.com>
2013-01-30 15:48:53 +0000

    Module for testing the scatter overlay

---
+++ jasmin_cis/test/manual_integration_tests/test_scatter_overlay.py
@@ -0,0 +1,17 @@
+from jasmin_cis.plot import Plotter
+from jasmin_cis.data_io.read import read_variable_from_files
+from jasmin_cis.test.test_util.mock import gen_random_data, gen_random_x, gen_random_y, ScatterData
+
+x1 = [gen_random_x(), gen_random_x(), gen_random_x(), gen_random_x(), gen_random_x()]
+x2 = [gen_random_x(), gen_random_x(), gen_random_x(), gen_random_x(), gen_random_x()]
+y1 = [gen_random_y(), gen_random_y(), gen_random_y(), gen_random_y(), gen_random_y()]
+y2 = [gen_random_y(), gen_random_y(), gen_random_y(), gen_random_y(), gen_random_y()]
+data = [gen_random_data(), gen_random_data(), gen_random_data(), gen_random_data(), gen_random_data()]
+scatter_data1 = ScatterData(x1, y1, data, (len(x1), len(y1)), "Scatter 1")
+scatter_data2 = ScatterData(x2, y2, None, (len(x2), len(y2)), "Scatter 2")
+heatmap_cube =  read_variable_from_files("/home/shared/NetCDF Files/xglnwa.pm.k8dec-k9nov.col.tm.nc", "rain")
+
+Plotter([heatmap_cube, scatter_data1, scatter_data2], datafiles = [{"itemstyle" : None, "color" : None, "label": None}, 
+                                                                   {"itemstyle" : "*", "color" : None, "label": "Scatter 1"}, 
+                                                                   {"itemstyle" : "p", "color" : "black", "label": "Scatter 2"}],
+                                                                     itemwidth = 30, cbarorient = "vertical")
