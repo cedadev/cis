92fe63e2b4289acaa69de35baa5267e529d6c9ef
Daniel Wallis <daniel.wallis@tessella.com>
2013-01-31 16:11:39 +0000

    Make methods private

--- jasmin_cis/data_io/write_netcdf.py
+++ jasmin_cis/data_io/write_netcdf.py
@@ -3,7 +3,7 @@ Module for writing data to NetCDF files
 '''
 from netCDF4 import Dataset
 
-def create_dimensions(nc_file, data_object):
+def __create_dimensions(nc_file, data_object):
     dimensions = ()
     for i, coord in enumerate(data_object.coords()):
         nc_file.createDimension(coord.name(), len(data_object.data_list[i]))
@@ -17,7 +17,7 @@ def create_dimensions(nc_file, data_object):
 def write_to_file(data_object, filename):
     # Create file
     netcdf_file = Dataset(filename, 'w')
-    dimensions = create_dimensions(netcdf_file, data_object)
+    dimensions = __create_dimensions(netcdf_file, data_object)
     variable = netcdf_file.createVariable(data_object.short_name, data_object.type, dimensions)
     variable[:] = data_object.data
     netcdf_file.close()
