3ed46db909471b564bbce5eb167cd87394f7002f
Duncan Watson-Parris <duncan.watson-parris@tessella.com>
2013-02-01 10:51:07 +0000

    Changed to create a masked array which pcolormesh is able to plot.

--- jasmin_cis/data_io/hdf_util.py
+++ jasmin_cis/data_io/hdf_util.py
@@ -4,5 +4,7 @@ Utility functions used by both hdf_sd and hdf_vd
 import numpy as np
 
 def __fill_missing_data(data, missing_val):
-    w_fill_mask = np.where(data == missing_val, np.nan, 1)
-    return data * w_fill_mask
+    import numpy.ma as ma
+    #w_fill_mask = np.where(data == missing_val, np.nan, 1)
+    #temp = data * w_fill_mask
+    return ma.array(data,mask=data==missing_val) 
