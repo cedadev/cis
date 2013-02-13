8cdadc45b10ad15063e7986054ea86fbdaf6cd4d
Daniel Wallis <daniel.wallis@tessella.com>
2013-02-13 17:12:37 +0000

    Rename dict to prevent confusion with data type

--- jasmin_cis/cis.py
+++ jasmin_cis/cis.py
@@ -56,12 +56,12 @@ def plot_cmd(main_arguments):
     
     try:
         # create a dictionary of [key=variable, value=list of filename]
-        dict={}
+        dict_of_var_and_filename={}
         for datafile in main_arguments["datafiles"]:
-            utils.add_element_to_list_in_dict(dict, datafile["variable"], datafile["filename"])
+            utils.add_element_to_list_in_dict(dict_of_var_and_filename, datafile["variable"], datafile["filename"])
 
         # create a list of data object (ungridded or gridded(in that case, a Iris cube)), concatenating data from various files
-        data = [ read_data(files,var) for var, files in dict.iteritems() ]
+        data = [ read_data(files,var) for var, files in dict_of_var_and_filename.iteritems() ]
 
     except (IrisError, ex.InvalidVariableError, ex.FileIOError) as e:
         __error_occurred(e)
