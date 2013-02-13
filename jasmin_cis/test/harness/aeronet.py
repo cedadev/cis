89b26e08513363b5089268eef0e546358c92d8b4
Daniel Wallis <daniel.wallis@tessella.com>
2013-02-13 14:20:21 +0000

    Plot aeronet

--- jasmin_cis/test/harness/aeronet.py
+++ jasmin_cis/test/harness/aeronet.py
@@ -1,6 +1,8 @@
 from jasmin_cis.data_io.products.AProduct import get_data
 
 filenames = ["/home/daniel/Aeronet_Files/Data/920801_091128_Abracos_Hill.lev20"]
-data_object = get_data(filenames, 'AOT_1640',"Aeronet")
+data_object = get_data(filenames, 'AOT_1020',"Aeronet")
 
-print data_object
+from jasmin_cis.plot import Plotter
+
+Plotter([data_object])
