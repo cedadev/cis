e4e0df5ef81c8cfcf91db54d5be270e487794e97
Daniel Wallis <daniel.wallis@tessella.com>
2013-02-13 14:49:34 +0000

    Overlay aeronet plots

--- jasmin_cis/test/harness/aeronet.py
+++ jasmin_cis/test/harness/aeronet.py
@@ -1,8 +1,12 @@
 from jasmin_cis.data_io.products.AProduct import get_data
 
 filenames = ["/home/daniel/Aeronet_Files/Data/920801_091128_Abracos_Hill.lev20"]
-data_object = get_data(filenames, 'AOT_1020',"Aeronet")
+data_object1 = get_data(filenames, 'AOT_1020',"Aeronet")
+data_object2 = get_data(filenames, 'AOT_870',"Aeronet")
+data_object3 = get_data(filenames, 'AOT_675',"Aeronet")
+data_object4 = get_data(filenames, 'AOT_500',"Aeronet")
+data_object5 = get_data(filenames, 'Watercm',"Aeronet")
 
 from jasmin_cis.plot import Plotter
 
-Plotter([data_object])
+Plotter([data_object1, data_object2, data_object3, data_object4, data_object5])
