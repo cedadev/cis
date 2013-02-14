8f89a76a14d4c35cdd33e1a315fc356d4f97585e
Duncan Watson-Parris <duncan.watson-parris@tessella.com>
2013-02-14 12:09:35 +0000

    Changes to the way colocated values are stored. Also cheated the input sample points to speed up development - to be changed back later

--- jasmin_cis/cis.py
+++ jasmin_cis/cis.py
@@ -109,9 +109,15 @@ def col_cmd(main_arguments):
 
     sample_file = main_arguments.pop("samplefilename")
     input_groups = main_arguments.pop("datafiles")
-    output_file = main_arguments.pop("output")
+    output_file = "cis-col-" + main_arguments.pop("output") + ".nc"
 
     coords = read_file_coordinates(sample_file)
+
+    for coord in coords:
+        print coord.data
+        coord.data = coord.data[0:10]
+        print coord.data
+
     sample_points = coords.get_coordinates_points()
 
     write_coordinates(coords, output_file)
