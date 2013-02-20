c476c99ac469366e88dd168d9a25f54e7bae88cb
Daniel Wallis <daniel.wallis@tessella.com>
2013-02-20 16:58:33 +0000

    Fix test harness for Cloud CCI

--- jasmin_cis/test/harness/cloud_cci.py
+++ jasmin_cis/test/harness/cloud_cci.py
@@ -1,11 +1,13 @@
 '''
 This is currently not  working due to files having incorrect fill values and potentially other problems too
 '''
-from jasmin_cis.data_io.products.AProduct import get_data
+#from jasmin_cis.data_io.products.AProduct import get_data
+from jasmin_cis.data_io.read import read_data
 
 #filenames = "/home/daniel/CCI_Files/*"
-filenames = [ "/home/daniel/CCI_Files/Modis_L2/20080620000000-ESACCI-L2_CLOUD-CLD_PRODUCTS-MODIS-AQUA-fv1.0.nc" ]
-data_object = get_data(filenames, 'stemp', "Cloud_CCI")
+#filenames = [ "/home/daniel/CCI_Files/Modis_L2/20080620000000-ESACCI-L2_CLOUD-CLD_PRODUCTS-MODIS-AQUA-fv1.0.nc" ]
+filenames = [ "/home/daniel/Downloads/Fixed_CCI_Files_From_Gareth/20080620072500-ESACCI-L2_CLOUD-CLD_PRODUCTS-MODIS-AQUA-fv1.0.nc"]
+data_object = read_data(filenames, 'stemp', "Cloud_CCI")
 
 from jasmin_cis.plot import Plotter
 
