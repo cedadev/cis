3d77115523f8de18b6f1f5ce2bbd9e0017db0a34
Daniel Wallis <daniel.wallis@tessella.com>
2013-02-20 16:38:30 +0000

    Use read instead of get_data

--- jasmin_cis/test/harness/caliop.py
+++ jasmin_cis/test/harness/caliop.py
@@ -1,8 +1,9 @@
-from jasmin_cis.data_io.products.AProduct import get_data
+from jasmin_cis.data_io.read import read_data# .products.AProduct import get_data
 
 filename = "/home/daniel/Caliop_Files/CAL_LID_L2_05kmAPro-Prov-V3-01.2009-12-31T23-36-08ZN.hdf"
 filenames = [filename]
-data_object = get_data(filenames, "Backscatter_Coefficient_1064", "Caliop")
+data_object = read_data(filenames, "Backscatter_Coefficient_1064", "Caliop")
+#data_object = get_data(filenames, "Backscatter_Coefficient_1064", "Caliop")
 
 # Eds code from jasmin_cis.test.harness.hdf import read_hdf4
 # Eds code datad = read_hdf4(filename) #DELETE ME
