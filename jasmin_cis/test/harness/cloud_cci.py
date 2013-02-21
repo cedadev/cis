94fa8204a6a816589db3b96649ab2405ead019a0
Daniel Wallis <daniel.wallis@tessella.com>
2013-02-21 14:40:28 +0000

    Use itemwidth of 1 when plotting cloud cci

--- jasmin_cis/test/harness/cloud_cci.py
+++ jasmin_cis/test/harness/cloud_cci.py
@@ -3,11 +3,11 @@ from jasmin_cis.data_io.read import read_data
 from glob import glob
 
 
-#filenames = glob("/home/daniel/Downloads/Fixed_CCI_Files_From_Gareth/*")
+filenames = glob("/home/daniel/Downloads/Fixed_CCI_Files_From_Gareth/*")
 #filenames = [ "/home/daniel/CCI_Files/Modis_L2/20080620000000-ESACCI-L2_CLOUD-CLD_PRODUCTS-MODIS-AQUA-fv1.0.nc" ]
-filenames = [ "/home/daniel/Downloads/Fixed_CCI_Files_From_Gareth/20080620073500-ESACCI-L2_CLOUD-CLD_PRODUCTS-MODIS-AQUA-fv1.0.nc"]
+#filenames = [ "/home/daniel/Downloads/Fixed_CCI_Files_From_Gareth/20080620073500-ESACCI-L2_CLOUD-CLD_PRODUCTS-MODIS-AQUA-fv1.0.nc"]
 data_object = read_data(filenames, 'stemp', "Cloud_CCI")
 
 from jasmin_cis.plot import Plotter
 
-Plotter([data_object], valrange = {"vmin" :260, "vmax" : 340}, plot_type="scatter")#"heatmap")
+Plotter([data_object], valrange = {"vmin" :260, "vmax" : 340}, plot_type="scatter", itemwidth=1)
