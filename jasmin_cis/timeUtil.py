4ae9114f83bc0f6219b36a543a3f4107adf6d471
David Michel <david.michel@tessella.com>
2013-02-27 16:52:26 +0000

    fixed time conversion

--- jasmin_cis/timeUtil.py
+++ jasmin_cis/timeUtil.py
@@ -5,15 +5,16 @@ import datetime as dt
 import numpy as np
 from datetime import timedelta
 
-def convert_tai_to_obj_array(tai_time_array):
-    for x in np.nditer(tai_time_array, op_flags=['readwrite']):
-        x[...] = convert_tai_to_obj(x,dt.datetime(1993,1,1))
+def convert_tai_to_obj_array(tai_time_array,ref):
+    converted_time = np.zeros(tai_time_array.shape, dtype='O')
+    for i,t in np.ndenumerate(tai_time_array):
+        converted_time[i] = convert_tai_to_obj(int(t),ref)
+    return converted_time
 
 def convert_tai_to_obj(tai_time, ref):
     return timedelta(seconds=tai_time) + ref
 
-
-a = np.arange(6).reshape(2,3)
-print a
-convert_tai_to_obj_array(a)
-print a
+# a = np.arange(6).reshape(2,3)
+# print a
+# b= convert_tai_to_obj_array(a,dt.datetime(1993,1,1))
+# print b
