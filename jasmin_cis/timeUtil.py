0dc5c44460b069335efe57f6cdf029355af85fa8
David Michel <david.michel@tessella.com>
2013-02-28 10:52:52 +0000

    added time conversion utilities

--- jasmin_cis/timeUtil.py
+++ jasmin_cis/timeUtil.py
@@ -14,7 +14,5 @@ def convert_tai_to_obj_array(tai_time_array,ref):
 def convert_tai_to_obj(tai_time, ref):
     return timedelta(seconds=tai_time) + ref
 
-# a = np.arange(6).reshape(2,3)
-# print a
-# b= convert_tai_to_obj_array(a,dt.datetime(1993,1,1))
-# print b
+
+
