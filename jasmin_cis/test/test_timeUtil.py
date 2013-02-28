4532043aa29fcbe010509f0ebd9af5d058b4d75f
Duncan Watson-Parris <duncan.watson-parris@tessella.com>
2013-02-28 16:08:46 +0000

    Added a time since util

--- jasmin_cis/test/test_timeUtil.py
+++ jasmin_cis/test/test_timeUtil.py
@@ -5,12 +5,12 @@ from nose.tools import istest, eq_
 import numpy as np
 import datetime as dt
 
-from jasmin_cis.timeUtil import convert_tai_to_obj_array
+from jasmin_cis.timeUtil import convert_sec_since_to_obj_array
 
 @istest
 def test_that_can_convert_tai_to_datetime_obj():
     a = np.arange(6).reshape(2,3)
-    b= convert_tai_to_obj_array(a,dt.datetime(1993,1,1))
+    b= convert_sec_since_to_obj_array(a,dt.datetime(1993,1,1))
     eq_(a.shape,b.shape)
     eq_(b[0][0],dt.datetime(1993,1,1,0,0,0))
     eq_(b[0][1],dt.datetime(1993,1,1,0,0,1))
