80fd412fd963dece439246620fd461cb4d939759
David Michel <david.michel@tessella.com>
2013-03-01 10:21:52 +0000

    added function to parse a time delta into a dateutil.relativedelta object

--- jasmin_cis/test/test_timeUtil.py
+++ jasmin_cis/test/test_timeUtil.py
@@ -1,7 +1,7 @@
 """
 module to test the time conversion utilities
 """
-from nose.tools import istest, eq_
+from nose.tools import istest, raises, eq_
 import numpy as np
 import datetime as dt
 
@@ -21,13 +21,32 @@ def test_that_can_convert_tai_to_datetime_obj():
 
 
 @istest
-def test_that_needs_renaming():
+def test_that_can_parse_datetimestr_to_obj():
+    # when not specifying the hours, minutes or seconds, 0 is used
     eq_(parse_datetimestr_to_obj("2010-02-05 02:15:45"),dt.datetime(2010,02,05,02,15,45))
     eq_(parse_datetimestr_to_obj("2010-02-05 02:15"),dt.datetime(2010,02,05,02,15,0))
     eq_(parse_datetimestr_to_obj("2010-02-05 02"),dt.datetime(2010,02,05,02,0,0))
     eq_(parse_datetimestr_to_obj("2010-02-05"),dt.datetime(2010,02,05,0,0,0))
 
-@istest
-def test_that_needs_renaming():
+    # GOTCHA: when not specifying an element of a date (i.e. the year, month or day), the current date is used
+    now = dt.datetime.now()
     eq_(parse_datetimestr_to_obj("2010-02-05"),dt.datetime(2010,02,05))
-    eq_(parse_datetimestr_to_obj("2010-02"),dt.datetime(2010,02,))
+    eq_(parse_datetimestr_to_obj("2010-02"),dt.datetime(2010,02,now.day))
+    eq_(parse_datetimestr_to_obj("2010-"),dt.datetime(2010,now.month,now.day))
+    eq_(parse_datetimestr_to_obj(""),dt.datetime(now.year,now.month,now.day))
+
+@istest
+def test_that_can_parse_time_deltas():
+    delta = parse_datetimestr_delta_to_obj("2y15m3d")
+    eq_(delta.years,3)
+    eq_(delta.months,3)
+    eq_(delta.days,3)
+    eq_(delta.hours,0)
+    eq_(delta.minutes,0)
+    eq_(delta.seconds,0)
+
+@istest
+@raises(ValueError)
+def test_that_raise_an_error_when_datetimestr_delta_is_invalid():
+    parse_datetimestr_delta_to_obj("some wierd string")
+
