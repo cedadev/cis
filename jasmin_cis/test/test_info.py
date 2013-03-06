7788a84e3bd96912e61f670793c604ff3b03e0a8
Daniel Wallis <daniel.wallis@tessella.com>
2013-03-06 10:45:30 +0000

    Test info command for modis l2

--- jasmin_cis/test/test_info.py
+++ jasmin_cis/test/test_info.py
@@ -17,12 +17,20 @@ def should_raise_error_when_file_is_not_netcdf():
     info(non_netcdf_file_with_netcdf_file_extension)
 
 @istest
-def can_get_info_on_an_hdf_file():
+def can_get_info_on_a_cloudsat_file():
     info(valid_cloudsat_RVOD_file)
 
 @istest
-def can_get_info_on_a_variable_in_an_hdf_file():
+def can_get_info_on_a_variable_in_a_cloudsat_file():
     info(valid_cloudsat_RVOD_file, [valid_cloudsat_RVOD_variable])
+
+@istest
+def can_get_info_on_a_modis_file():
+    info(valid_modis_l2_filename)
+
+@istest
+def can_get_info_on_a_variable_in_a_modis_file():
+    info(valid_modis_l2_filename, [valid_modis_l2_variable])
 
 @istest
 def can_get_info_on_an_aeronet_file():
