cc82b17f0bd4073159724b8b3213609371af4cb4
Daniel Wallis <daniel.wallis@tessella.com>
2013-03-08 10:15:42 +0000

    Set version number to V0R4M2 in cis.py

--- jasmin_cis/cis.py
+++ jasmin_cis/cis.py
@@ -10,7 +10,7 @@ from jasmin_cis.exceptions import CISError
 logger = logging.getLogger(__name__)
 
 __author__ = "David Michel, Daniel Wallis and Duncan Watson-Parris"
-__version__ = "x"
+__version__ = "V0R4M2"
 __status__ = "Development"
 __website__ = "http://proj.badc.rl.ac.uk/cedaservices/wiki/JASMIN/CommunityIntercomparisonSuite"
 
