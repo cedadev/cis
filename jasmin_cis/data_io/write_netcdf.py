f31432daa76dfc0f16973f2384a82f7f4479e015
Daniel Wallis <daniel.wallis@tessella.com>
2013-03-08 11:48:43 +0000

    Write colocation history to file

--- jasmin_cis/data_io/write_netcdf.py
+++ jasmin_cis/data_io/write_netcdf.py
@@ -20,6 +20,7 @@ def __add_metadata(var, data):
     if data.metadata.range : var.valid_range = data.metadata.range
     if data.metadata.missing_value : var.missing_value = data.metadata.missing_value
     if data.metadata.calendar : var.calendar = data.metadata.calendar
+    if data.metadata.history : var.history = data.metadata.history
     return var
 
 
