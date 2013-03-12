c18f444529ef7c723f51aeb5dbf45939c84ad9d8
Daniel Wallis <daniel.wallis@tessella.com>
2013-03-12 15:09:46 +0000

    Fix tests

--- jasmin_cis/test/test_parse_col.py
+++ jasmin_cis/test/test_parse_col.py
@@ -12,14 +12,16 @@ def can_specify_one_valid_samplefile_and_one_complete_datagroup():
     args = ["col", valid_1d_filename, "variable:"+valid_1d_filename+":col:con:nn"]
     args = parse_args(args)
     eq_(valid_1d_filename, args.samplegroup)
-    eq_([{"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : ("col",{}), "constraint" : ("con",{}), "kernel" : ("nn",{}) }], args.datagroups)
+    eq_([{"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : ("col",{}), "constraint" : ("con",{}), "kernel" : ("nn",{}), "product" : None}],
+        args.datagroups)
 
 @istest
 def can_specify_one_valid_samplefile_and_one_datafile_without_other_options():
     args = ["col", valid_1d_filename, "variable:"+valid_1d_filename]
     args = parse_args(args)
     eq_(valid_1d_filename, args.samplegroup)
-    eq_([{"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : None, "constraint" : None, "kernel" : None}], args.datagroups)
+    eq_([{"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : None, "constraint" : None, "kernel" : None, "product" : None}],
+        args.datagroups)
 
 @istest
 def can_specify_one_valid_samplefile_and_many_datagroups():
@@ -31,11 +33,12 @@ def can_specify_one_valid_samplefile_and_many_datagroups():
             "variable:"+valid_1d_filename+':col::nn']
     args = parse_args(args)
     eq_(valid_1d_filename, args.samplegroup)
-    eq_([{"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : None, "constraint" : None, "kernel" : None},
-         {"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : ('col',{}), "constraint" : None, "kernel" : None},
-         {"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : None, "constraint" : ('con',{}), "kernel" : None},
-         {"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : None, "constraint" : None, "kernel" : ('nn',{})},
-         {"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : ('col',{}), "constraint" : None, "kernel" : ('nn',{})}], args.datagroups)
+    eq_([{"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : None, "constraint" : None, "kernel" : None, "product" : None},
+         {"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : ('col',{}), "constraint" : None, "kernel" : None, "product" : None},
+         {"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : None, "constraint" : ('con',{}), "kernel" : None, "product" : None},
+         {"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : None, "constraint" : None, "kernel" : ('nn',{}), "product" : None},
+         {"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : ('col',{}), "constraint" : None, "kernel" : ('nn',{}), "product" : None}],
+        args.datagroups)
 
 
 @istest
@@ -44,7 +47,7 @@ def can_specify_one_valid_samplefile_and_one_datafile_with_internal_options():
     args = parse_args(args)
     eq_(valid_1d_filename, args.samplegroup)
     eq_([{"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : None,
-          "constraint" : ('SepConstraint',{'h_sep':'1500','v_sep':'22000','t_sep':'5000'}), "kernel" : ('nn',{})}], args.datagroups)
+          "constraint" : ('SepConstraint',{'h_sep':'1500','v_sep':'22000','t_sep':'5000'}), "kernel" : ('nn',{}), "product" : None}], args.datagroups)
 
 
 @istest
