a2495b10d0bb8249cc9f69d612262b033298f324
Daniel Wallis <daniel.wallis@tessella.com>
2013-03-12 14:58:39 +0000

    Fix tests

--- jasmin_cis/test/test_parse_col.py
+++ jasmin_cis/test/test_parse_col.py
@@ -11,14 +11,14 @@ from jasmin_cis.test.test_files.data import *
 def can_specify_one_valid_samplefile_and_one_complete_datagroup():
     args = ["col", valid_1d_filename, "variable:"+valid_1d_filename+":col:con:nn"]
     args = parse_args(args)
-    eq_(valid_1d_filename, args.samplefilename)
+    eq_(valid_1d_filename, args.samplegroup)
     eq_([{"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : ("col",{}), "constraint" : ("con",{}), "kernel" : ("nn",{}) }], args.datagroups)
 
 @istest
 def can_specify_one_valid_samplefile_and_one_datafile_without_other_options():
     args = ["col", valid_1d_filename, "variable:"+valid_1d_filename]
     args = parse_args(args)
-    eq_(valid_1d_filename, args.samplefilename)
+    eq_(valid_1d_filename, args.samplegroup)
     eq_([{"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : None, "constraint" : None, "kernel" : None}], args.datagroups)
 
 @istest
@@ -30,7 +30,7 @@ def can_specify_one_valid_samplefile_and_many_datagroups():
             "variable:"+valid_1d_filename+':::nn',
             "variable:"+valid_1d_filename+':col::nn']
     args = parse_args(args)
-    eq_(valid_1d_filename, args.samplefilename)
+    eq_(valid_1d_filename, args.samplegroup)
     eq_([{"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : None, "constraint" : None, "kernel" : None},
          {"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : ('col',{}), "constraint" : None, "kernel" : None},
          {"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : None, "constraint" : ('con',{}), "kernel" : None},
@@ -42,7 +42,7 @@ def can_specify_one_valid_samplefile_and_many_datagroups():
 def can_specify_one_valid_samplefile_and_one_datafile_with_internal_options():
     args = ["col", valid_1d_filename, "variable:"+valid_1d_filename+"::SepConstraint,h_sep=1500,v_sep=22000,t_sep=5000:nn"]
     args = parse_args(args)
-    eq_(valid_1d_filename, args.samplefilename)
+    eq_(valid_1d_filename, args.samplegroup)
     eq_([{"filenames" : [valid_1d_filename], "variable" : "variable", "colocator" : None,
           "constraint" : ('SepConstraint',{'h_sep':'1500','v_sep':'22000','t_sep':'5000'}), "kernel" : ('nn',{})}], args.datagroups)
 
