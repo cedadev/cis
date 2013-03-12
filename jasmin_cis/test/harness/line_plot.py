d1cccd4591e50e8dbe13e533a48571b3715b0955
Daniel Wallis <daniel.wallis@tessella.com>
2013-03-12 12:29:30 +0000

    Update test harness

--- jasmin_cis/test/harness/line_plot.py
+++ jasmin_cis/test/harness/line_plot.py
@@ -5,5 +5,6 @@ data_object2 = make_dummy_1d_ungridded_data()
 Plotter([data_object1, data_object2],
         datagroups = [{"itemstyle" : None, "color" : None, "label" : "Line 1"},
                                         {"itemstyle" : None, "color" : None, "label" : None}],
-        xrange = {"xmin" : -100, "xstep" : 25},
+        xrange = {"xmin" : -100, "xstep" : 25, "xmax" : 100},
+        yrange = {"ymin" : 0, "ystep" : 0.5},
         plot_type="line")
