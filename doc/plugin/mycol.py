afd0189cecd55c09e7532427a7e2c812567db72c
Duncan Watson-Parris <duncan.watson-parris@tessella.com>
2013-03-14 16:58:56 +0000

    Adding example colocation plugin file.

---
+++ doc/plugin/mycol.py
@@ -0,0 +1,37 @@
+from jasmin_cis.col_framework import Colocator, Constraint, Kernel
+from jasmin_cis.data_io.ungridded_data import LazyData
+
+
+class MyColocator(Colocator):
+
+    def colocate(self, points, data, constraint, kernel):
+        values = []
+        for point in points:
+            con_points = constraint.constrain_points(point, data)
+            try:
+                values.append(kernel.get_value(point, con_points))
+            except ValueError:
+                values.append(constraint.fill_value)
+        new_data = LazyData(values, data.metadata)
+        new_data.missing_value = constraint.fill_value
+        return new_data
+
+
+class MyConstraint(Constraint):
+
+    def constrain_points(self, ref_point, data):
+        con_points = []
+        for point in data:
+            if point.value > self.val_check:
+                con_points.append(point)
+        return con_points
+
+
+class MyKernel(Kernel):
+
+    def get_value(self, point, data):
+        nearest_point = point.furthest_point_from()
+        for data_point in data:
+            if point.compdist(nearest_point, data_point):
+                nearest_point = data_point
+        return nearest_point.val
