f6f612a5d2142d1cd2d79f3bd562aa2624dd6303
Stephen pascoe <Stephen.Pascoe@stfc.ac.uk>
2014-01-20 14:47:14 +0000

    Misnamed import

--- jasmin_cis/test/__init__.py
+++ jasmin_cis/test/__init__.py
@@ -1,5 +1,5 @@
 
 # Ensure test data is linked from JASMIN
 #!TODO: package test data in a manner that is relocatable
-from jasmin_cis.test.test_data import link_test_data
+from jasmin_cis.test.test_files import link_test_data
 link_test_data()
