3f1e15852c78057ed76bfd874d0e4b79bd904989
Ian Bush <ian.bush@tessella.com>
2014-01-31 17:57:06 +0000

    Revert "Attempt again to fix plotting on Jenkins and other environments where $DISPLAY is not set."
    This reverts commit f82b4964c22af8d5783c3140e737a88e27243727.

--- jasmin_cis/cis.py
+++ jasmin_cis/cis.py
@@ -144,10 +144,6 @@ def main():
     Sets up logging, parses the command line arguments and then calls the appropriate command with its arguments
     '''
     import os
-    import matplotlib
-    # We need to set Agg before calling pyplot if no $DISPLAY environmental variable exists
-    if os.environ.get('DISPLAY') is None:
-        matplotlib.use('Agg')
     from parse import parse_args
     import logging, logging.config
     from datetime import datetime
