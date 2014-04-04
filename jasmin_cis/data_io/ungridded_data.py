917929a8894a3a41cc520d7bcc4dc0ca4ace5f1e
Ian Bush <ian.bush@tessella.com>
2014-04-04 11:24:56 +0100

    Fix for outputting data when doing gridded -> ungridded colocation, where the gridded data has no standard_name.

--- jasmin_cis/data_io/ungridded_data.py
+++ jasmin_cis/data_io/ungridded_data.py
@@ -18,7 +18,7 @@ class Metadata(object):
 
     @classmethod
     def from_CubeMetadata(cls, cube_meta):
-        return cls(name='',standard_name=cube_meta.standard_name,long_name=cube_meta.long_name, units=str(cube_meta.units), misc=cube_meta.attributes)
+        return cls(name=cube_meta.var_name,standard_name=cube_meta.standard_name,long_name=cube_meta.long_name, units=str(cube_meta.units), misc=cube_meta.attributes)
 
 
     def __init__(self, name='', standard_name='', long_name='', shape='', units='', range='', factor='', offset='', missing_value='', calendar='', history = '', misc=None):
