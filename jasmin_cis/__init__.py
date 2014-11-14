044d9438ba62cc6e2c5e49b56b26f74d8cf48805
mathew-kendall-tessella <matthew.kendall@tessella.com>
2014-11-14 16:03:53 +0000

    Merge with feature branch

--- jasmin_cis/__init__.py
+++ jasmin_cis/__init__.py
@@ -1,6 +1,6 @@
 
-__author__ = "David Michel, Daniel Wallis, Duncan Watson-Parris, Richard Wilkinson and Ian Bush"
-__version__ = "0.7"
-__status__ = "Phase 2, end of phase release"
+__author__ = "David Michel, Daniel Wallis, Duncan Watson-Parris, Richard Wilkinson, Ian Bush, Matt Kendall"
+__version__ = "0.7.2"
+__status__ = "Phase 3, Sprint 2 release"
 __website__ = "http://proj.badc.rl.ac.uk/cedaservices/wiki/JASMIN/CommunityIntercomparisonSuite"
 
