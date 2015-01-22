4474a082d5042cdc8a21a595f5295def1aafb45b
John-Holt-Tessella <John.Holt@tessella.com>
2015-01-22 17:14:09 +0000

    Remove import from kernel

--- jasmin_cis/col_implementations.py
+++ jasmin_cis/col_implementations.py
@@ -294,11 +294,10 @@ class mean(Kernel):
         '''
             Colocation using the mean of any points left after a constraint.
         '''
-        from numpy import mean
         values = data.vals
         if len(values) == 0:
             raise ValueError
-        return mean(values)
+        return np.mean(values)
 
 
 class stddev(Kernel):
