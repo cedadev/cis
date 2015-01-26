c8e705334f36082dfc0586abec2cd24bf15288d5
mathew-kendall-tessella <matthew.kendall@tessella.com>
2015-01-26 13:29:07 +0000

    JASCIS-102: Remove standard deviation kernel

--- jasmin_cis/aggregation/aggregation_kernels.py
+++ jasmin_cis/aggregation/aggregation_kernels.py
@@ -65,5 +65,4 @@ class MultiKernel(object):
 aggregation_kernels = {'mean': iris.analysis.MEAN,
                        'min': iris.analysis.MIN,
                        'max': iris.analysis.MAX,
-                       'stddev': iris.analysis.STD_DEV,
                        'moments': MultiKernel('moments', [iris.analysis.MEAN, StddevKernel(), CountKernel()])}
