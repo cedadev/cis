cedadev/cis

commit 13b948ae46004da2ea73e23166fbfb3c715ababa

JASCIS-103: Aeronet product stops numpy from deleting characters from aeronet variable names (e.g. 'Datetime(dd-mm-yy)' was becoming 'Datetimeddmmyy'). See this incredibly frustrating bug: https://github.com/numpy/numpy/pull/4649

jasmin_cis/data_io/aeronet.py
@@ -1,8 +1,11 @@
 import logging
 
+defaultdeletechars = """~!@#$%^&*=+~\|]}[{'; /?.>,<"""
+
+
 def get_aeronet_file_variables(filename):
     import linecache
-    defaultdeletechars = """~!@#$%^&*()-=+~\|]}[{';: /?.>,<"""
+
     vars = linecache.getline(filename, 5).split(",")
     for i in range(0, len(vars)):
         for char in defaultdeletechars:
@@ -24,9 +27,9 @@ def load_multiple_aeronet(fnames, variables=None):
 
         # reading in all variables into a dictionary:
         # a_dict, key: variable name, value: list of masked arrays
-        a_dict = load_aeronet(filename,variables)
+        a_dict = load_aeronet(filename, variables)
         for var in a_dict.keys():
-            add_element_to_list_in_dict(adata,var,a_dict[var])
+            add_element_to_list_in_dict(adata, var, a_dict[var])
 
     for var in adata.keys():
         adata[var] = concatenate(adata[var])
@@ -49,7 +52,8 @@ def load_aeronet(fname, variables=None):
     from numpy import ma
     from datetime import datetime, timedelta
 
-    std_day = datetime(1900,1,1,0,0,0)
+    std_day = datetime(1900, 1, 1, 0, 0, 0)
+
     def date2daynum(datestr):
         the_day = datetime.strptime(datestr, '%d:%m:%Y')
         return float((the_day - std_day).days)
@@ -62,15 +66,16 @@ def load_aeronet(fname, variables=None):
         return std_day + timedelta(days=int(daynum), seconds=int(seconds))
 
     try:
-        rawd = np.genfromtxt(fname, skip_header=4, delimiter=',', names=True,
-                             converters={0:date2daynum, 1:time2seconds}, missing_values='N/A', usemask=True)
+        rawd = np.genfromtxt(fname, skip_header=4, delimiter=',', names=True, deletechars=defaultdeletechars,
+                             dtype=None, converters={0: date2daynum, 1: time2seconds}, missing_values='N/A',
+                             usemask=True)
     except (StopIteration, IndexError) as e:
         raise IOError(e)
 
     lend = len(rawd)
     dates = np.zeros(lend, dtype='O')
     for i in xrange(lend):
-        dates[i] = daynum_seconds2datetime(rawd['Dateddmmyy'][i], rawd['Timehhmmss'][i])
+        dates[i] = daynum_seconds2datetime(rawd['Date(dd-mm-yy)'][i], rawd['Time(hh:mm:ss)'][i])
 
     metadata = get_file_metadata(fname)
     lon = np.zeros(lend) + float(metadata.misc[2][1].split("=")[1])
@@ -90,13 +95,15 @@ def load_aeronet(fname, variables=None):
     return data_dict
 
 
-def get_file_metadata(filename, variable = '', shape = None):
+def get_file_metadata(filename, variable='', shape=None):
     file = open(filename)
     from jasmin_cis.data_io.ungridded_data import Metadata
-    if variable is None: variable = ''
-    metadata = Metadata(name = variable, long_name = variable, shape = shape)
+
+    if variable is None:
+        variable = ''
+    metadata = Metadata(name=variable, long_name=variable, shape=shape)
     lines = []
     for i in range(0, 4):
-        lines.append(file.readline().replace("\n","").split(","))
+        lines.append(file.readline().replace("\n", "").split(","))
     metadata.misc = lines
     return metadata

commit 86c38cd5f5f06b8a15d1ab62e04406bce222816e

JASCIS-132: Fix bug introduced in 13b948ae46004da2ea73e23166fbfb3c715ababa where providing dtype=None to numpy.genfromtxt made the output datatypes wrong and couldn't be written to file.

jasmin_cis/data_io/aeronet.py
@@ -55,7 +55,10 @@ def load_aeronet(fname, variables=None):
     std_day = datetime(1900, 1, 1, 0, 0, 0)
 
     def date2daynum(datestr):
-        the_day = datetime.strptime(datestr, '%d:%m:%Y')
+        try:
+            the_day = datetime.strptime(datestr, '%d:%m:%Y')
+        except ValueError:
+            the_day = datetime.strptime(datestr, '%d/%m/%Y')
         return float((the_day - std_day).days)
 
     def time2seconds(timestr):
@@ -65,10 +68,24 @@ def load_aeronet(fname, variables=None):
     def daynum_seconds2datetime(daynum, seconds):
         return std_day + timedelta(days=int(daynum), seconds=int(seconds))
 
+    def convert_datatypes_to_floats(column):
+        """
+        Numpy.genfromtext() converts missing values 'N/A' to floats when using dtype=None.
+        Also converts dates to objects.
+        Convert them into float64 as they should be.
+        :param column: Column to convert if a boolean.
+        :return:
+        """
+        if column.dtype == np.bool:
+            return column.astype(np.float64)
+        if column.dtype == np.object:
+            return np.array([column], dtype=np.float64)
+        return column
+
     try:
         rawd = np.genfromtxt(fname, skip_header=4, delimiter=',', names=True, deletechars=defaultdeletechars,
-                             dtype=None, converters={0: date2daynum, 1: time2seconds}, missing_values='N/A',
-                             usemask=True)
+                             converters={0: date2daynum, 1: time2seconds, 'Last_Processing_Date': date2daynum},
+                             dtype=None, missing_values='N/A', usemask=True)
     except (StopIteration, IndexError) as e:
         raise IOError(e)
 
@@ -85,7 +102,7 @@ def load_aeronet(fname, variables=None):
     data_dict = {}
     if variables is not None:
         for key in variables:
-            data_dict[key] = rawd[key]
+            data_dict[key] = convert_datatypes_to_floats(rawd[key])
 
     data_dict["datetime"] = ma.array(dates)
     data_dict["longitude"] = ma.array(lon)

jasmin_cis/test/integration/test_subset.py
@@ -187,12 +187,12 @@ class TestTemporalSubsetAllProductsNamedVariables(BaseIntegrationTest):
 
     def test_subset_Aeronet(self):
         # Takes 30s
-        variable = 'Dateddmmyy,Timehhmmss,Julian_Day,AOT_1640,AOT_1020,AOT_870,AOT_675,AOT_667,AOT_555,AOT_551,' \
-                   'AOT_532,AOT_531,AOT_500,AOT_490,AOT_443,AOT_440,AOT_412,AOT_380,AOT_340,Watercm,TripletVar_1640,' \
+        variable = 'Date(dd-mm-yy),Julian_Day,AOT_1640,AOT_1020,AOT_870,AOT_675,AOT_667,AOT_555,AOT_551,' \
+                   'AOT_532,AOT_531,AOT_500,AOT_490,AOT_443,AOT_440,AOT_412,AOT_380,AOT_340,Water(cm),TripletVar_1640,' \
                    'TripletVar_1020,TripletVar_870,TripletVar_675,TripletVar_667,TripletVar_555,TripletVar_551,' \
                    'TripletVar_532,TripletVar_531,TripletVar_500,TripletVar_490,TripletVar_443,TripletVar_440,' \
-                   'TripletVar_412,TripletVar_380,TripletVar_340,WaterError,440870Angstrom,380500Angstrom,' \
-                   '440675Angstrom,500870Angstrom,340440Angstrom,440675AngstromPolar,Last_Processing_Date,' \
+                   'TripletVar_412,TripletVar_380,TripletVar_340,WaterError,440-870Angstrom,380-500Angstrom,' \
+                   '440-675Angstrom,500-870Angstrom,340-440Angstrom,440-675Angstrom(Polar),Last_Processing_Date,' \
                    'Solar_Zenith_Angle'
         filename = valid_aeronet_filename
         time_min, time_max = '2003-09-24T07:00:00', '2003-11-04T07:00:00'