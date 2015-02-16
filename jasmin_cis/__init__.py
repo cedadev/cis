e239e0eee6062f7fe2f6076e40d6e228fafe17e1
mathew-kendall-tessella <matthew.kendall@tessella.com>
2015-02-16 18:51:04 +0000

    Update version number for 0.8.0 (mid-sprint 8) release

--- jasmin_cis/__init__.py
+++ jasmin_cis/__init__.py
@@ -1,5 +1,5 @@
 
 __author__ = "David Michel, Daniel Wallis, Duncan Watson-Parris, Richard Wilkinson, Ian Bush, Matt Kendall, John Holt"
-__version__ = "0.7.5"
-__status__ = "Phase 3, Sprint 7 release"
+__version__ = "0.8.0"
+__status__ = "Phase 3, Sprint 8 Interim release"
 __website__ = "http://proj.badc.rl.ac.uk/cedaservices/wiki/JASMIN/CommunityIntercomparisonSuite"
