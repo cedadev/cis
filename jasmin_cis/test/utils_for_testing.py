deecb515a406fd922237bc27201765d715526b75
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2015-07-07 09:28:10 +0100

    Gave test_aggregation_over_time a higher tolerance for acceptance for float comparison as it was very low: 10^-15

--- jasmin_cis/test/utils_for_testing.py
+++ jasmin_cis/test/utils_for_testing.py
@@ -1,13 +1,14 @@
 from hamcrest import assert_that, is_
 import numpy
 
+
 def assert_arrays_equal(result, expected):
         assert_that(numpy.array_equal(result, expected), is_(True),
                 "arrays not the same. Expected\n {}\n was\n {}".format(expected, result))
 
 
-def assert_arrays_almost_equal(result, expected):
-        assert_that(numpy.allclose(result, expected, atol=1.0e-15)
+def assert_arrays_almost_equal(result, expected, tol=1.0e-15):
+        assert_that(numpy.allclose(result, expected, atol=tol)
                     , is_(True),
                     "arrays not almost the same. Expected\n {}\n was\n {}".format(expected, result))
 
