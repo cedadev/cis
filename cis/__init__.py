917bb3d3ff84daa5c93b75e75d178550c1242d82
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2015-07-23 16:48:10 +0100

    Updating version and tagging

--- cis/__init__.py
+++ cis/__init__.py
@@ -1,5 +1,5 @@
 
 __author__ = "David Michel, Daniel Wallis, Duncan Watson-Parris, Richard Wilkinson, Ian Bush, Matt Kendall, John Holt"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __status__ = "Stable"
 __website__ = "http://proj.badc.rl.ac.uk/cedaservices/wiki/JASMIN/CommunityIntercomparisonSuite"
