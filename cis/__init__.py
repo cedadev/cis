032cc89ee9c011ef560c6635f172cc237e0ae9dd
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2015-08-11 15:14:41 +0100

    Updated URLs to point to new website

--- cis/__init__.py
+++ cis/__init__.py
@@ -2,4 +2,4 @@
 __author__ = "David Michel, Daniel Wallis, Duncan Watson-Parris, Richard Wilkinson, Ian Bush, Matt Kendall, John Holt"
 __version__ = "1.1.2"
 __status__ = "Stable"
-__website__ = "http://proj.badc.rl.ac.uk/cedaservices/wiki/JASMIN/CommunityIntercomparisonSuite"
+__website__ = "http://www.cistools.net/"
