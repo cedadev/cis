560684ee4816acba522113863e48d1a5c28b6cc7
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2015-08-19 11:28:35 +0100

    Fix import

--- cis/data_io/products/__init__.py
+++ cis/data_io/products/__init__.py
@@ -11,7 +11,7 @@ from NCAR_NetCDF_RAF import NCAR_NetCDF_RAF
 from MODIS import MODIS_L2, MODIS_L3
 from caliop import abstract_Caliop, Caliop_L1, Caliop_L2
 from CCI import Aerosol_CCI, Cloud_CCI
-from HadGEM import HadGEM_PP
+from HadGEM import HadGEM_PP, HadGEM_CONVSH
 
 #list of all data products
 __all__ = [
