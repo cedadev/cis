1372646208f9d331e9ea98e0bcd1ae3e61f3c69f
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2015-09-07 12:36:10 +0100

    Updated docstrings

--- cis/info.py
+++ cis/info.py
@@ -3,22 +3,20 @@ Routines for the info command
 '''
 
 
-def info(filenames, user_variables=None, product=None, data_type=None):
+def info(filenames, variables=None, product=None, type=None):
     '''
-    Read all the variables from a file and print to stdout.
-    File can contain either gridded and ungridded data.
-    First tries to read data as gridded, if that fails, tries as ungridded.
+    Read all the variables from a file, or a specified selection of variables in more detail, and print to stdout.
 
     :param filenames:   The filenames of the files to read
-    :param user_variables:   The user specified variables of interest
-    :param data_type:   String representing the type of HDF data to read, i.e. 'VD' or 'SD'
-
+    :param variables:   The user specified variables of interest
+    :param product:   The data reading plugin to use when reading the data
+    :param type:   String representing the type of HDF data to read, i.e. 'VD' or 'SD'
     '''
     from cis.data_io.products.AProduct import get_variables, get_data
 
-    if user_variables is not None:
-        for user_var in user_variables:
+    if variables is not None:
+        for user_var in variables:
             print str(get_data(filenames, user_var, product=product))
     else:
-        for variable in get_variables(filenames, product=product, data_type=data_type):
+        for variable in get_variables(filenames, product=product, data_type=type):
             print variable
