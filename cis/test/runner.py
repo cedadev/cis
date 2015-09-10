4ad53d9203a3a4833712a9e337c408821a50722e
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2015-09-10 13:44:08 +0100

    Optionally output debugging information when running tests

--- cis/test/runner.py
+++ cis/test/runner.py
@@ -1,10 +1,17 @@
 from setuptools.command.test import test as TestCommand
 import multiprocessing
 
-def run(test_set='cis/test/unit', n_processors=1, stop=False):
-    import nose
 
-    args = ['', test_set, '--processes=%s' % n_processors, '--verbosity=2',]
+def run(test_set='cis/test/unit', n_processors=1, stop=False, debug=False):
+    import nose
+    import logging, sys
+
+    if debug:
+        logging.basicConfig(level=logging.DEBUG,
+                            format="%(asctime)s - %(levelname)s - %(module)s : %(lineno)d - %(message)s",
+                            stream=sys.stdout)
+
+    args = ['', test_set, '--processes=%s' % n_processors, '--verbosity=2']
 
     if stop:
         args.append('--stop')
@@ -20,13 +27,15 @@ class nose_test(TestCommand):
                   " be run instead."
     user_options = [('integration-tests', 'i', 'Run the integration tests.'),
                     ('stop', 'x', 'Stop running tests after the first error or failure.'),
-                    ('num-processors=', 'p', 'The number of processors used for running the tests.')]
+                    ('num-processors=', 'p', 'The number of processors used for running the tests.'),
+                    ('debug', 'd', 'Send debug level logging information to stdout.')]
 
     def initialize_options(self):
         TestCommand.initialize_options(self)
         self.integration_tests = False
         self.stop = False
         self.num_processors = None
+        self.debug = False
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
@@ -43,7 +52,7 @@ class nose_test(TestCommand):
             self.num_processors = int(self.num_processors)
 
     def run_tests(self):
-        run(self.test_set, self.num_processors, self.stop)
+        run(self.test_set, self.num_processors, self.stop, self.debug)
 
         # nose.run_exit(argv=['nosetests',os.path.join(os.path.dirname(__file__), 'unit')])
 
