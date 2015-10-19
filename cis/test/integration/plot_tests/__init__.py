5ba61e825a7079d626823c2e7d81807cbeab752e
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2015-10-19 21:01:31 +0100

    Added python versions of the plotting integration tests, along with a matplotlib check to see if the output matches a reference within a given tolerance (heavily inspired by iris)

---
+++ cis/test/integration/plot_tests/__init__.py
@@ -0,0 +1,4 @@
+"""
+Plotting integration tests. These tests use matplotlib to actually check the image output is 'similar' to some reference
+image. The tolerance is defined in the test_plotting script and based onthe default used in iris 1.8.0.
+"""
