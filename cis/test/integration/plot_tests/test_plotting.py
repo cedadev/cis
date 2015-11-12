faffef14a602f48cc2d5bc5a1560e2007a9494bb
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2015-11-12 22:18:49 +0000

    Fixed a couple of tests

--- cis/test/integration/plot_tests/test_plotting.py
+++ cis/test/integration/plot_tests/test_plotting.py
@@ -484,7 +484,8 @@ class TestPlotVisual(VisualTest):
 
         Plotter(datas, x_variable='longitude', y_variable='latitude', plotwidth=8, plotheight=6, cbarscale=None,
                 itemwidth=50, yrange={'ymin':-90, 'ymax':90}, xrange={'xmin':0,'xmax':360},
-                datagroups=[{'itemstyle':'', 'cmap':'jet', 'edgecolor':''}])
+                datagroups=[{'itemstyle':'', 'cmap':'jet', 'edgecolor':'', 'label': '-180to180'},
+                            {'itemstyle':'', 'cmap':'jet', 'edgecolor':'', 'label': '0to360'}])
 
         self.check_graphic()
 
@@ -498,9 +499,10 @@ class TestPlotVisual(VisualTest):
         datas = [make_regular_2d_ungridded_data(lat_dim_length=2, lon_dim_length=90, lon_min=-175., lon_max=145.),
                  make_regular_2d_ungridded_data(lon_dim_length=90, lon_min=5., lon_max=325., lat_min=30, lat_max=60)]
 
-        Plotter([datas], x_variable='longitude', y_variable='latitude', plotwidth=8, plotheight=6, cbarscale=None,
+        Plotter(datas, x_variable='longitude', y_variable='latitude', plotwidth=8, plotheight=6, cbarscale=None,
                 itemwidth=50, yrange={'ymin':-90, 'ymax':90}, xrange={'xmin':-180,'xmax':180},
-                datagroups=[{'itemstyle':'', 'cmap':'jet', 'edgecolor':''}])
+                datagroups=[{'itemstyle':'', 'cmap':'jet', 'edgecolor':'', 'label': '-180to180'},
+                            {'itemstyle':'', 'cmap':'jet', 'edgecolor':'', 'label': '0to360'}])
 
         self.check_graphic()
 
