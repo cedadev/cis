2132605a00e5bba1808c17e56638738e1cd12311
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2016-01-04 09:32:37 +0000

    Updated the plot test paths and moved the accept/reject buttons so they aren't off the bottom of the screen.

--- cis/test/plot_tests/idiff.py
+++ cis/test/plot_tests/idiff.py
@@ -41,8 +41,8 @@ def diff_viewer(expected_fname, result_fname, diff_fname):
         print('REJECTED: %s' % (os.path.basename(expected_fname), ))
         plt.close()
 
-    ax_accept = plt.axes([0.7, 0.05, 0.1, 0.075])
-    ax_reject = plt.axes([0.81, 0.05, 0.1, 0.075])
+    ax_accept = plt.axes([0.6, 0.35, 0.1, 0.075])
+    ax_reject = plt.axes([0.71, 0.35, 0.1, 0.075])
     bnext = mwidget.Button(ax_accept, 'Accept change')
     bnext.on_clicked(accept)
     bprev = mwidget.Button(ax_reject, 'Reject')
@@ -52,14 +52,14 @@ def diff_viewer(expected_fname, result_fname, diff_fname):
 
 
 def step_over_diffs():
-    import cis.test.integration.plot_tests
-    image_dir = os.path.join(os.path.dirname(cis.test.integration.plot_tests.__file__),
+    import cis.test.plot_tests
+    image_dir = os.path.join(os.path.dirname(cis.test.plot_tests.__file__),
                              'reference', 'visual_tests')
-    diff_dir = os.path.join(os.path.dirname(cis.test.integration.plot_tests.__file__),
+    diff_dir = os.path.join(os.path.dirname(cis.test.plot_tests.__file__),
                             'result_image_comparison')
 
     for expected_fname in sorted(os.listdir(image_dir)):
-        result_path = os.path.join(diff_dir, 'result-' + expected_fname)
+        result_path = os.path.join(diff_dir, expected_fname)
         diff_path = result_path[:-4] + '-failed-diff.png'
 
         # if the test failed, there will be a diff file
