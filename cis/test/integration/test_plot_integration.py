97f18d6dc971223dbd1a8f1bcfae9d7e97a2197f
Duncan Watson-Parris <duncan@watson-parris.co.uk>
2016-08-05 09:33:44 +0100

    Explicitly set the product to test so that it doesn't pick up any local ones

--- cis/test/integration/test_plot_integration.py
+++ cis/test/integration/test_plot_integration.py
@@ -13,7 +13,7 @@ class TestPlotIntegration(BaseIntegrationTest):
 
     def test_should_do_scatter_plot_of_file_valid_aerosol_cci_file(self):
         # Actual file name: 20080612093821-ESACCI-L2P_AEROSOL-ALL-AATSR_ENVISAT-ORAC_32855-fv02.02.nc
-        arguments = ['plot', valid_aerosol_cci_variable+':'+valid_aerosol_cci_filename, '--output',
+        arguments = ['plot', valid_aerosol_cci_variable+':'+escape_colons(valid_aerosol_cci_filename), '--output',
                      valid_aerosol_cci_filename+'.png']
         main_arguments = parse_args(arguments)
         plot_cmd(main_arguments)
@@ -23,7 +23,7 @@ class TestPlotIntegration(BaseIntegrationTest):
 
     def test_should_do_scatter_plot_of_valid_2d_file(self):
         # Actual file name: xglnwa.pm.k8dec-k9nov.col.tm.nc
-        arguments = ['plot', valid_1d_variable+':'+valid_2d_filename, '--output', valid_2d_filename+'.png']
+        arguments = ['plot', valid_1d_variable+':'+escape_colons(valid_2d_filename), '--output', valid_2d_filename+'.png']
         main_arguments = parse_args(arguments)
         plot_cmd(main_arguments)
 
@@ -33,7 +33,7 @@ class TestPlotIntegration(BaseIntegrationTest):
     def test_should_do_scatter_plot_of_valid_2d_file_with_x_as_lat_y_as_long(self):
         # Actual file name: xglnwa.pm.k8dec-k9nov.col.tm.nc
         # Trac issue #153
-        arguments = ['plot', valid_1d_variable+':'+valid_2d_filename, '--xaxis', 'latitude', '--yaxis', 'longitude',
+        arguments = ['plot', valid_1d_variable+':'+escape_colons(valid_2d_filename), '--xaxis', 'latitude', '--yaxis', 'longitude',
                      '--output', valid_2d_filename+'.png']
         main_arguments = parse_args(arguments)
         plot_cmd(main_arguments)
@@ -44,7 +44,7 @@ class TestPlotIntegration(BaseIntegrationTest):
     def test_should_do_heatmap_plot_of_valid_1d_file(self):
         # Actual file name: xglnwa.pm.k8dec-k9nov.vprof.tm.nc
         # Trac issue #145
-        arguments = ['plot', valid_1d_variable+':'+valid_1d_filename, '--output',
+        arguments = ['plot', valid_1d_variable+':'+escape_colons(valid_1d_filename), '--output',
                      valid_1d_filename+'.png']
         main_arguments = parse_args(arguments)
         plot_cmd(main_arguments)
@@ -56,7 +56,7 @@ class TestPlotIntegration(BaseIntegrationTest):
         # Actual file name: rsutcs_Amon_HadGEM2-A_sstClim_r1i1p1_185912-188911.CMIP5.tm.zm.nc
         # Trac issue #130
         arguments = ['plot',
-                     valid_zonal_time_mean_CMIP5_variable+':'+valid_zonal_time_mean_CMIP5_filename,
+                     valid_zonal_time_mean_CMIP5_variable+':'+escape_colons(valid_zonal_time_mean_CMIP5_filename),
                      '--output', valid_zonal_time_mean_CMIP5_filename+'.png']
         main_arguments = parse_args(arguments)
         plot_cmd(main_arguments)
@@ -67,7 +67,7 @@ class TestPlotIntegration(BaseIntegrationTest):
     def test_should_do_plot_of_hybrid_height_when_formula_terms_not_marked_as_coordinates(self):
         # Actual file name: hybrid-height.nc
         # Trac issue #417 (fixed in IRIS v.1.7.1)
-        arguments = ['plot', valid_hybrid_height_flat_variable+':'+valid_hybrid_height_flat_filename,
+        arguments = ['plot', valid_hybrid_height_flat_variable+':'+escape_colons(valid_hybrid_height_flat_filename),
                      '--output', valid_hybrid_height_filename+'.png']
         main_arguments = parse_args(arguments)
         plot_cmd(main_arguments)
@@ -78,7 +78,7 @@ class TestPlotIntegration(BaseIntegrationTest):
     def test_should_do_plot_of_hybrid_pressure_using_calculated_air_pressure_variable(self):
         # Actual file name: hybrid-pressure.nc
         # Trac issue #416 (fixed in IRIS v.1.7.1)
-        arguments = ['plot', valid_hybrid_pressure_variable+':'+valid_hybrid_pressure_filename,
+        arguments = ['plot', valid_hybrid_pressure_variable+':'+escape_colons(valid_hybrid_pressure_filename),
                      '--xaxis', 'air_pressure', '--output', valid_hybrid_pressure_filename+'.png']
         main_arguments = parse_args(arguments)
         plot_cmd(main_arguments)
@@ -89,11 +89,11 @@ class TestPlotIntegration(BaseIntegrationTest):
     def test_subset_ECHAM_over_0_360_boundary_plots_OK(self):
         var = valid_echamham_variable_1
         filename = valid_echamham_filename
-        args = ['subset', var + ':' + escape_colons(filename), 'x=[-10,10]', '-o', self.OUTPUT_FILENAME]
+        args = ['subset', var + ':' + escape_colons(filename) + ":product=NetCDF_Gridded", 'x=[-10,10]', '-o', self.OUTPUT_FILENAME]
         args = parse_args(args)
         subset_cmd(args)
         out_name = 'subset_echam_boundary.png'
-        args = ['plot', var + ":" + self.OUTPUT_FILENAME, '--type', 'contourf', '-o', out_name]
+        args = ['plot', var + ":" + self.OUTPUT_FILENAME + ":product=NetCDF_Gridded", '--type', 'contourf', '-o', out_name]
         args = parse_args(args)
         plot_cmd(args)
 
@@ -104,22 +104,23 @@ class TestPlotIntegration(BaseIntegrationTest):
         variable = valid_cis_gridded_output_variable
         filename = valid_cis_gridded_output_filename
         out_name = '3d_out.png'
-        args = ['plot', variable + ':' + escape_colons(filename), '-o', out_name]
+        args = ['plot', variable + ':' + escape_colons(filename) + ":product=NetCDF_Gridded", '-o', out_name]
         main_arguments = parse_args(args)
         plot_cmd(main_arguments)
 
     def test_plot_gridded_2d_with_flattened_time(self):
         variable = valid_cis_gridded_output_variable
         filename = valid_cis_gridded_output_filename
-        args = ['subset', variable + ':' + escape_colons(filename), 't=[2007-06-07T15]', '-o', self.OUTPUT_FILENAME]
+        args = ['subset', variable + ':' + escape_colons(filename) + ":product=NetCDF_Gridded",
+                't=[2007-06-07T15]', '-o', self.OUTPUT_FILENAME]
         args = parse_args(args)
         subset_cmd(args)
         out_name = '3d_out.png'
-        args = ['plot', variable + ':' + self.OUTPUT_FILENAME, '-o', out_name]
+        args = ['plot', variable + ':' + self.OUTPUT_FILENAME + ":product=NetCDF_Gridded", '-o', out_name]
         main_arguments = parse_args(args)
         plot_cmd(main_arguments)
 
-        # os.remove(out_name)
+        os.remove(out_name)
 
     def test_plot_aggregated_aeronet(self):
         # Aggregated aeronet has multiple length 1 dimensions, so we want to make sure we can plot it OK.
