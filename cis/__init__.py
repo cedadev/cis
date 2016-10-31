a93a60f91b74c85291bc995d1298f18c472eb59b
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2016-10-31 11:57:10 +0000

    Add a top-level routine for listing the variables in a set of files

--- cis/__init__.py
+++ cis/__init__.py
@@ -25,7 +25,7 @@ __version__ = "1.5.0"
 __status__ = "Dev"
 __website__ = "http://www.cistools.net/"
 
-__all__ = ['read_data', 'read_data_list']
+__all__ = ['read_data', 'read_data_list', 'get_variables']
 
 
 def read_data(filenames, variable, product=None):
@@ -72,3 +72,27 @@ def read_data_list(filenames, variables, product=None, aliases=None):
     if len(file_set) == 0:
         raise IOError("No files found which match: {}".format(filenames))
     return DataReader().read_data_list(expand_filelist(filenames), variables, product, aliases)
+
+
+def get_variables(filenames, product=None, type=None):
+    """
+    Get a list of variables names from a list of files. Files can be either gridded or ungridded but not a mix of both.
+
+    :param filenames:   The filenames of the files to read. This can be either a single filename as a string, a comma
+     separated list, or a :class:`list` of string filenames. Filenames can include directories which will be expanded to
+     include all files in that directory, or wildcards such as ``*`` or ``?``.
+    :type filenames: string or list
+    :param str product: The name of the data reading plugin to use to read the data (e.g. ``Cloud_CCI``).
+    :param str type: The type of HDF data to read, i.e. 'VD' or 'SD'
+    :return:  A list of the variables
+    """
+    from cis.data_io.data_reader import expand_filelist
+    from cis.data_io.products.AProduct import get_variables
+
+    try:
+        file_set = expand_filelist(filenames)
+    except ValueError as e:
+        raise IOError(e)
+    if len(file_set) == 0:
+        raise IOError("No files found which match: {}".format(filenames))
+    return get_variables(filenames, product=product, data_type=type)
